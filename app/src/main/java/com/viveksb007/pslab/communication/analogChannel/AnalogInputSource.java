b72e07dc602e4380c283be87051f8a84df737f9c
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-03-26 02:39:36 +0530

    Digital channel added

--- app/src/main/java/com/viveksb007/pslab/communication/analogChannel/AnalogInputSource.java
+++ app/src/main/java/com/viveksb007/pslab/communication/analogChannel/AnalogInputSource.java
@@ -14,22 +14,22 @@ public class AnalogInputSource {
     boolean gainEnabled = false, inverted = false, caliberationReady = false;
     double gain = 0;
     int gainPGA, inversion = 1, defaultOffsetCode = 0, scaling = 1;
-    private String name;
+    private String channelName;
 
 
-    public AnalogInputSource(String name) {
+    public AnalogInputSource(String channelName) {
         AnalogConstants analogConstants = new AnalogConstants();
-        this.name = name;
-        range = analogConstants.inputRanges.get(name);
+        this.channelName = channelName;
+        range = analogConstants.inputRanges.get(channelName);
         if (range[1] - range[0] < 0) {
             inverted = true;
             inversion = -1;
         }
-        if (name.equals("CH1")) {
+        if (channelName.equals("CH1")) {
             gainEnabled = true;
             gainPGA = 1;
             gain = 0;
-        } else if (name.equals("CH2")) {
+        } else if (channelName.equals("CH2")) {
             gainEnabled = true;
             gainPGA = 2;
             gain = 0;
@@ -40,7 +40,7 @@ public class AnalogInputSource {
 
     boolean setGain(int index) {
         if (!gainEnabled) {
-            Log.e(TAG, "Analog gain is not available on " + name);
+            Log.e(TAG, "Analog gain is not available on " + channelName);
             return false;
         }
         gain = gainValues[index];
