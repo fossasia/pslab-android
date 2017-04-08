d5a642a85e4651c80f1d407d960bc073375c25b9
Vivek Singh <viveksbhadauria007@gmail.com>
2017-04-08 22:54:08 +0530

    updated upload-apk.sh (#35)

--- upload-apk.sh
+++ upload-apk.sh
@@ -10,7 +10,7 @@ cd $HOME
 git config --global user.email "noreply@travis.com"
 git config --global user.name "Travis CI" 
 #clone the repository in the buildApk folder
-git clone --quiet --branch=apk https://fossasia:$GITHUB_API_KEY@github.com/fossasia/pslab-android apk > /dev/null
+git clone --quiet --branch=apk https://viveksb007:$GITHUB_API_KEY@github.com/fossasia/pslab-android apk > /dev/null
 cp -Rf $HOME/buildApk/*
 cd apk
 
