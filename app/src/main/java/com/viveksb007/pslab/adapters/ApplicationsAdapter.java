97123ad4cdc5bc656902057d18eaafebbecdc6e0
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-04-10 00:36:50 +0530

    reformatted and cleaned code

--- app/src/main/java/com/viveksb007/pslab/adapters/ApplicationsAdapter.java
+++ app/src/main/java/com/viveksb007/pslab/adapters/ApplicationsAdapter.java
@@ -1,4 +1,4 @@
-package com.viveksb007.pslab.fragment;
+package com.viveksb007.pslab.adapters;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -14,47 +14,50 @@ import com.viveksb007.pslab.R;
  * Created by akarshan on 4/9/17.
  */
 
-public class CustomAdapter extends BaseAdapter {
-    private static LayoutInflater inflater=null;
-    private String [] applicationsList;
-    private int [] imageList;
-    public CustomAdapter(Context context, String[] list1, int[] list2) {
-        applicationsList=list1;
-        imageList = list2;
-        inflater = (LayoutInflater)context.
+public class ApplicationsAdapter extends BaseAdapter {
+
+    private static LayoutInflater inflater = null;
+    private String[] applicationsList;
+    private int[] imageList;
+
+    public ApplicationsAdapter(Context context, String[] applicationsList, int[] imageList) {
+        this.applicationsList = applicationsList;
+        this.imageList = imageList;
+        inflater = (LayoutInflater) context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
+
     @Override
     public int getCount() {
-        // TODO Auto-generated method stub
         return applicationsList.length;
     }
 
     @Override
     public Object getItem(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 
     @Override
     public long getItemId(int position) {
-        // TODO Auto-generated method stub
         return position;
     }
 
-    private class Holder
-    {
+    private class Holder {
         TextView tv;
         ImageView img;
     }
+
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
-        // TODO Auto-generated method stub
-        Holder holder=new Holder();
+        Holder holder = new Holder();
         View rowView;
-        rowView = inflater.inflate(R.layout.applications_list, null);
-        holder.tv=(TextView) rowView.findViewById(R.id.application_tv);
-        holder.img=(ImageView) rowView.findViewById(R.id.application_icon);
+        if (convertView == null) {
+            rowView = inflater.inflate(R.layout.applications_list_item, null);
+        } else {
+            rowView = convertView;
+        }
+        holder.tv = (TextView) rowView.findViewById(R.id.application_tv);
+        holder.img = (ImageView) rowView.findViewById(R.id.application_icon);
         holder.tv.setText(applicationsList[position]);
         holder.img.setImageResource(imageList[position]);
         return rowView;
