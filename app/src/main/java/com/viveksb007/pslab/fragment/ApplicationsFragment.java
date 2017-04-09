fea5e8c42da3bc42a4192d28e463747d25ef75a2
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-04-09 23:34:16 +0530

    Rebuilt Applications Fragment according to ListView approach (#37)
    * ListView approach of Application Fragment
    
    Used ListView instead of earlier image-button approach.
    
    * Created applications_list.xml
    
    This file defines the design of rows of the list used in applications fragment.
    
    * Updated ApplicationsFragment.java (ListView)
    
    The changes were made according to ListView approach.
    
    * Created CustomAdapter
    
    Custom Adapter for ListView of Applications Fragment.

--- app/src/main/java/com/viveksb007/pslab/fragment/ApplicationsFragment.java
+++ app/src/main/java/com/viveksb007/pslab/fragment/ApplicationsFragment.java
@@ -1,20 +1,23 @@
 package com.viveksb007.pslab.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.ListView;
 import com.viveksb007.pslab.R;
+
 
 /**
  * Created by viveksb007 on 29/3/17.
  */
 
 public class ApplicationsFragment extends Fragment {
-
+    String [] applicationsList = {"Oscilloscope","Control","Advance Control","Logical Analyzer","Data Sensor Logger","Wireless Sensor","Sensor QuickView","Wireless Sensor QuickView"};
+    int [] imageList = {R.drawable.osciloscope,R.drawable.control,R.drawable.advance,R.drawable.la,R.drawable.sensor,R.drawable.wirelesssensor,R.drawable.sensor_quickview,R.drawable.wireless_sensor_quickview};
     public static ApplicationsFragment newInstance() {
         ApplicationsFragment applicationsFragment = new ApplicationsFragment();
         return applicationsFragment;
@@ -22,8 +25,12 @@ public class ApplicationsFragment extends Fragment {
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.applications_fragment, container, false);
+        ListView applicationsListView = (ListView) view.findViewById(R.id.applicationsListView);
+        applicationsListView.setAdapter(new CustomAdapter(getContext(),applicationsList,imageList));
         return view;
+
     }
+    
 }
