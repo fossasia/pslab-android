be083d71bc7590d56a22a0a33b8f63dabe292aae
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-04-19 19:26:42 +0530

    Create MLX90614.java
    Ported MLX90614.py

---
+++ app/src/main/java/org/fossasia/pslab/communication/sensor/MLX90614.java
@@ -0,0 +1,95 @@
+package org.fossasia.pslab.communication.sensors;
+
+
+import android.util.Log;
+import org.fossasia.pslab.communication.peripherals.I2C;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * Created by akarshan on 4/17/17.
+ */
+
+public class MLX90614 {
+
+    private String TAG = "MLX90614";
+    private int ADDRESS = 0x5A;;
+
+    public int NUMPLOTS = 1;
+    public String[] PLOTNAMES = {"Temp"};
+    public String name = "PIR temperature";
+
+    private I2C i2c;
+    private int source, OBJADDR = 0x07, AMBADDR = 0x06;
+    private HashMap<String, ArrayList> params = new HashMap<>();
+
+    public MLX90614(I2C i2c) throws IOException {
+        this.i2c =i2c;
+        source = OBJADDR;
+        String name = "Passive IR temperature sensor";
+        try {
+            Log.d(TAG,"switching baud to 100k");
+            i2c.config((int)100e3);
+        }
+        catch (Exception e)
+        {
+            Log.d(TAG,"failed to cahnge baud rate");
+        }
+        ArrayList<Integer> _readReg_ = new ArrayList<>();
+        for(int i = 0;i < 0x20;i++)
+        {
+            _readReg_.add(i);
+        }
+
+        params.put("readReg", _readReg_);
+        params.put("selectSource", new ArrayList(Arrays.asList("object temperature","ambient temperature")) );
+    }
+
+    public void selectSource(String _source_)
+    {
+        if(_source_.equals("object temperature"))
+            source = OBJADDR;
+        else if (_source_.equals("ambient temperature"))
+            source = AMBADDR;
+    }
+
+    public void readReg(int addr) throws IOException {
+        ArrayList<Character> x = getVals(addr, 2);
+        Log.v(TAG, Integer.toHexString(addr)+" "+Integer.toHexString(x.get(0) | (x.get(1) << 8)));
+    }
+
+    public ArrayList<Character> getVals(int addr, int bytes) throws IOException {
+        ArrayList<Character> vals = i2c.readBulk(ADDRESS, addr, bytes);
+        return vals;
+    }
+
+    public Double[] getRaw() throws IOException {
+        ArrayList<Character> vals = getVals(source,3);
+        if (vals.size() == 3)
+            return new Double[]{((((vals.get(1) & 0x007f) << 8) + vals.get(0)) * 0.02) - 0.01 - 273.15};
+        else
+            return null;
+    }
+
+    public Double getObjectTemperature() throws IOException {
+        source = OBJADDR;
+        Double [] val = getRaw();
+        if (val.length != 0)
+            return val[0];
+        else
+            return null;  //returning null instead of false to match the return data-type. Used wrapper class Double because null cannot be returned on primitive data-type.
+    }
+
+    public Double getAmbientTemperature() throws IOException
+    {
+        source = AMBADDR;
+        Double[] val = getRaw();
+        if (val.length != 0)
+            return val[0];
+        else
+            return null;
+    }
+
+}
