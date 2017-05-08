600b6e68decacf8767cf6571dbbc50970f9e4e2d
CloudyPadmal <blog.padmal@gmail.com>
2017-05-08 23:02:38 +0530

    Fixes #92 : Applications view UI redesign!

---
+++ app/src/main/java/org/fossasia/pslab/items/SquareImageView.java
@@ -0,0 +1,31 @@
+package org.fossasia.pslab.items;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by Padmal on 5/8/17.
+ */
+
+public class SquareImageView extends ImageView {
+    public SquareImageView(Context context) {
+        super(context);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int width = getMeasuredWidth();
+        setMeasuredDimension(width, width);
+    }
+}
