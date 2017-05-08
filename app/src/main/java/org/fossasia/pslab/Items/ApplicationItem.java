92078b895b94b72eac2470eb51d2c6271935fa91
CloudyPadmal <blog.padmal@gmail.com>
2017-05-08 22:13:36 +0530

    Fixes #92 : Applications view UI redesign!

---
+++ app/src/main/java/org/fossasia/pslab/Items/ApplicationItem.java
@@ -0,0 +1,36 @@
+package org.fossasia.pslab.Items;
+
+/**
+ * Created by Padmal on 5/7/17.
+ */
+
+public class ApplicationItem {
+
+    private String applicationName;
+    private int applicationIcon;
+
+    // Default constructor
+    public ApplicationItem() {/**/}
+
+    public ApplicationItem(String applicationName, int applicaitonIcon) {
+        this.applicationName = applicationName;
+        this.applicationIcon = applicaitonIcon;
+    }
+
+    public String getApplicationName() {
+        return applicationName;
+    }
+
+    public void setApplicationName(String applicationName) {
+        this.applicationName = applicationName;
+    }
+
+    public int getApplicationIcon() {
+        return applicationIcon;
+    }
+
+    public void setApplicationIcon(int applicationIcon) {
+        this.applicationIcon = applicationIcon;
+    }
+
+}
