88d530648919c3a5a56f9a5f26374133b3fedf9a
Padmal <CloudyPadmal@users.noreply.github.com>
2017-05-09 15:17:34 +0000

    Fixes #92 : Applications view UI redesign! (#106)

---
+++ app/src/main/java/org/fossasia/pslab/adapters/ApplicationAdapter.java
@@ -0,0 +1,82 @@
+package org.fossasia.pslab.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.fossasia.pslab.items.ApplicationItem;
+import org.fossasia.pslab.R;
+
+import java.util.List;
+
+/**
+ * Created by Padmal on 5/7/17.
+ */
+
+public class ApplicationAdapter extends RecyclerView.Adapter<ApplicationAdapter.Holder> {
+
+    private Context mContext;
+    private List<ApplicationItem> applicationList;
+    private final OnItemClickListener listener;
+
+    /**
+     * View holder for application list item
+     */
+    public class Holder extends RecyclerView.ViewHolder {
+
+        public TextView applicationName;
+        public ImageView applicationIcon;
+
+        public Holder(View itemView) {
+            super(itemView);
+            this.applicationName = (TextView) itemView.findViewById(R.id.application_name);
+            this.applicationIcon = (ImageView) itemView.findViewById(R.id.application_icon);
+        }
+
+        public void setup(final ApplicationItem applicationItem, final OnItemClickListener listener) {
+            // Set application name
+            applicationName.setText(applicationItem.getApplicationName());
+            // Set application Icon
+            Picasso.with(mContext).load(applicationItem.getApplicationIcon()).into(applicationIcon);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {
+                    listener.onItemClick(applicationItem);
+                }
+            });
+        }
+    }
+
+    public ApplicationAdapter(Context mContext, List<ApplicationItem> applicationList, OnItemClickListener listener) {
+        this.mContext = mContext;
+        this.applicationList = applicationList;
+        this.listener = listener;
+    }
+
+    @Override
+    public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.application_list_item, parent, false);
+        return new Holder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(Holder holder, int position) {
+        holder.setup(applicationList.get(position), listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return applicationList.size();
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(ApplicationItem item);
+    }
+
+}
--- app/src/main/java/org/fossasia/pslab/fragment/ApplicationsFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/ApplicationsFragment.java
@@ -1,15 +1,24 @@
 package org.fossasia.pslab.fragment;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.Toast;
 
+import org.fossasia.pslab.items.ApplicationItem;
 import org.fossasia.pslab.R;
-import org.fossasia.pslab.adapters.ApplicationsAdapter;
+import org.fossasia.pslab.adapters.ApplicationAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -18,21 +27,102 @@ import org.fossasia.pslab.adapters.ApplicationsAdapter;
 
 public class ApplicationsFragment extends Fragment {
 
-    private String[] applicationsList = {"Oscilloscope", "Control", "Advance Control", "Logical Analyzer", "Data Sensor Logger", "Wireless Sensor", "Sensor QuickView", "Wireless Sensor QuickView"};
-    private int[] imageList = {org.fossasia.pslab.R.drawable.osc, org.fossasia.pslab.R.drawable.control, org.fossasia.pslab.R.drawable.advance, org.fossasia.pslab.R.drawable.la, org.fossasia.pslab.R.drawable.sensor, org.fossasia.pslab.R.drawable.wirelesssensor, org.fossasia.pslab.R.drawable.sensor_quickview, org.fossasia.pslab.R.drawable.wireless_sensor_quickview};
+    private ApplicationAdapter applicationAdapter;
+    private List<ApplicationItem> applicationItemList;
+    private Context context;
 
     public static ApplicationsFragment newInstance() {
-        ApplicationsFragment applicationsFragment = new ApplicationsFragment();
-        return applicationsFragment;
+        return new ApplicationsFragment();
     }
 
     @Nullable
     @Override
-    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.applications_fragment, container, false);
-        ListView applicationsListView = (ListView) view.findViewById(org.fossasia.pslab.R.id.applicationsListView);
-        applicationsListView.setAdapter(new ApplicationsAdapter(getContext(), applicationsList, imageList));
+        // Initiate context
+        context = getActivity().getApplicationContext();
+        // Initiate resources
+        applicationItemList = new ArrayList<>();
+        applicationAdapter = new ApplicationAdapter(context, applicationItemList,
+                new ApplicationAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ApplicationItem item) {
+                Toast.makeText(context, item.getApplicationName(),
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+        // Initiate Views
+        initiateViews(view);
+        // Generate Applications
+        new loadList().execute();
+        // Return created view
         return view;
+    }
+
+    /**
+     * Initiate Recycler view
+     */
+    private void initiateViews(View view) {
+        // Initiate Recycler View with a Grid
+        RecyclerView listView = (RecyclerView) view.findViewById(R.id.applications_recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(context, 2);
+        listView.setLayoutManager(mLayoutManager);
+        listView.setItemAnimator(new DefaultItemAnimator());
+        // Set adapter
+        listView.setAdapter(applicationAdapter);
+    }
+
+    /**
+     * Generate an array of Application Items and add them to the adapter in background
+     */
+    private class loadList extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            int[] applications = new int[]{
+                    R.drawable.osciloscope_icon,
+                    R.drawable.control_icon,
+                    R.drawable.advance_icon,
+                    R.drawable.logic_analzers_icon,
+                    R.drawable.sensor_icon,
+                    R.drawable.wireless_sensor_icon,
+                    R.drawable.sensor_qv_icon,
+                    R.drawable.wireless_sensor_qv_icon};
+
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.oscilloscope), applications[0])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.control), applications[1])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.advance_control), applications[2])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.logical_analyzer), applications[3])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.data_sensor_logger), applications[4])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.w_sensor_logger), applications[5])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.sensor_qview), applications[6])
+            );
+            applicationItemList.add(new ApplicationItem(
+                    getResources().getString(R.string.w_sensor_qview), applications[7])
+            );
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            // Notify the adapter that data has been fetched
+            applicationAdapter.notifyDataSetChanged();
+        }
     }
 
 }
