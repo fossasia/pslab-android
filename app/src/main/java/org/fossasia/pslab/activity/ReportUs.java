bd997e27c8d6fbb89b0bbdb65e79023920d841ec
Asitava Sarkar <asitavasarkar@gmail.com>
2017-05-21 15:12:51 +0530

    Fixes #116 implements butterknife
    Fixes #116 implements butterknife
    
    Fixes #116 implements butterknife
    
    Fixes #116 implements butterknife
    
    Fixes #116 implements butterknife

--- app/src/main/java/org/fossasia/pslab/activity/ReportUs.java
+++ app/src/main/java/org/fossasia/pslab/activity/ReportUs.java
@@ -5,6 +5,9 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import org.fossasia.pslab.R;
+
+import butterknife.ButterKnife;
+
 /**
  * Created by viveksb007 on 15/3/17.
  */
@@ -15,5 +18,6 @@ public class ReportUs extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report_us);
+        ButterKnife.bind(this);
     }
 }
--- app/src/main/java/org/fossasia/pslab/fragment/HomeFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/HomeFragment.java
@@ -11,6 +11,10 @@ import android.widget.TextView;
 
 import org.fossasia.pslab.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 /**
  * Created by viveksb007 on 15/3/17.
  */
@@ -19,6 +23,10 @@ public class HomeFragment extends Fragment {
 
     private boolean deviceFound = false, deviceConnected = false;
     private String version;
+    @BindView(R.id.tv_device_status) TextView tvDeviceStatus;
+    @BindView(R.id.tv_device_version) TextView tvVersion;
+    @BindView(R.id.img_device_status) ImageView imgViewDeviceStatus;
+    private Unbinder unbinder;
 
     public static HomeFragment newInstance(boolean deviceConnected, boolean deviceFound, String version) {
         HomeFragment homeFragment = new HomeFragment();
@@ -38,9 +46,7 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_fragment, container, false);
-        TextView tvDeviceStatus = (TextView) view.findViewById(org.fossasia.pslab.R.id.tv_device_status);
-        TextView tvVersion = (TextView) view.findViewById(org.fossasia.pslab.R.id.tv_device_version);
-        ImageView imgViewDeviceStatus = (ImageView) view.findViewById(org.fossasia.pslab.R.id.img_device_status);
+        unbinder = ButterKnife.bind(this,view);
         if (deviceFound & deviceConnected) {
             imgViewDeviceStatus.setImageResource(org.fossasia.pslab.R.drawable.usb_connected);
             tvDeviceStatus.setText("Device Connected Successfully");
@@ -50,5 +56,10 @@ public class HomeFragment extends Fragment {
             tvDeviceStatus.setText("PSLab Device not found");
         }
         return view;
+    }
+
+    @Override public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
