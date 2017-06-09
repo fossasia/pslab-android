a5c46dc06d4ca381267341fb11c856237a3893d3
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-06-09 22:17:46 +0530

    LALogicLinesFragment created

---
+++ app/src/main/java/org/fossasia/pslab/fragment/LALogicLinesFragment.java
@@ -0,0 +1,21 @@
+package org.fossasia.pslab.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by viveksb007 on 9/6/17.
+ */
+
+public class LALogicLinesFragment extends Fragment {
+
+    public static LALogicLinesFragment newInstance() {
+        LALogicLinesFragment laLogicLinesFragment = new LALogicLinesFragment();
+        return laLogicLinesFragment;
+    }
+
+}
