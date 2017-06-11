b7f3e96f3cb5ec35c9abce9258f49f9f2c5864dc
Asitava Sarkar <asitavasarkar@gmail.com>
2017-06-11 06:27:46 +0530

    Fixes #41 creates UI for Control
    Fixes #41 creates UI for Control

--- app/src/main/java/org/fossasia/pslab/activity/ControlActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/ControlActivity.java
@@ -1,12 +1,21 @@
 package org.fossasia.pslab.activity;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 import org.fossasia.pslab.communication.ScienceLab;
+import org.fossasia.pslab.fragment.ControlFragmentAdvanced;
+import org.fossasia.pslab.fragment.ControlFragmentMain;
+import org.fossasia.pslab.fragment.ControlFragmentRead;
 import org.fossasia.pslab.others.ScienceLabCommon;
 
+import org.fossasia.pslab.R;
 import butterknife.ButterKnife;
 
 /**
@@ -20,8 +29,35 @@ public class ControlActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView();
+        setContentView(R.layout.activity_control);
         scienceLab = ScienceLabCommon.getInstance().scienceLab;
-        ButterKnife.bind(this);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.action_item1:
+                                selectedFragment = ControlFragmentMain.newInstance();
+                                break;
+                            case R.id.action_item2:
+                                selectedFragment = ControlFragmentRead.newInstance();
+                                break;
+                            case R.id.action_item3:
+                                selectedFragment = ControlFragmentAdvanced.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, ControlFragmentMain.newInstance());
+        transaction.commit();
     }
 }
