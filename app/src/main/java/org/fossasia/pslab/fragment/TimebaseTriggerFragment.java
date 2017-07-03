b3eef3a9ebed15043450ce5195180b98a014425c
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-07-04 02:16:40 +0530

    Resolves typo error and provides checkbox for trigger

--- app/src/main/java/org/fossasia/pslab/fragment/TimebaseTriggerFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/TimebaseTriggerFragment.java
@@ -10,6 +10,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -18,7 +20,7 @@ import org.fossasia.pslab.R;
 import org.fossasia.pslab.activity.OscilloscopeActivity;
 import org.fossasia.pslab.others.FloatSeekBar;
 
-public class TimebaseTiggerFragment extends Fragment {
+public class TimebaseTriggerFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
@@ -29,13 +31,14 @@ public class TimebaseTiggerFragment extends Fragment {
     private OnFragmentInteractionListener mListener;
     private Spinner spinnerTiggerChannelSelect;
     private FloatSeekBar seekBarTimebase;
-    private FloatSeekBar seekBarTigger;
+    private FloatSeekBar seekBarTrigger;
     private TextView textViewTimeBase;
-    private TextView textViewTigger;
+    private TextView textViewTrigger;
+    private CheckBox checkBoxTrigger;
 
 
-    public static TimebaseTiggerFragment newInstance(String param1, String param2) {
-        TimebaseTiggerFragment fragment = new TimebaseTiggerFragment();
+    public static TimebaseTriggerFragment newInstance(String param1, String param2) {
+        TimebaseTriggerFragment fragment = new TimebaseTriggerFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,14 +63,16 @@ public class TimebaseTiggerFragment extends Fragment {
 
         //seekBarTimebase = (SeekBar) v.findViewById(R.id.seekBar_timebase_tt);
         seekBarTimebase = (FloatSeekBar) v.findViewById(R.id.seekBar_timebase_tt);
-        seekBarTigger = (FloatSeekBar) v.findViewById(R.id.seekBar_tigger);
+        seekBarTrigger = (FloatSeekBar) v.findViewById(R.id.seekBar_trigger);
         textViewTimeBase = (TextView) v.findViewById(R.id.tv_timebase_values_tt);
-        textViewTigger = (TextView) v.findViewById(R.id.tv_tigger_values_tt);
-        spinnerTiggerChannelSelect = (Spinner) v.findViewById(R.id.spinner_tigger_channel_tt);
+        textViewTrigger = (TextView) v.findViewById(R.id.tv_trigger_values_tt);
+        spinnerTiggerChannelSelect = (Spinner) v.findViewById(R.id.spinner_trigger_channel_tt);
+        checkBoxTrigger = (CheckBox) v.findViewById(R.id.checkbox_trigger_tt);
+
         boolean tabletSize = getResources().getBoolean(R.bool.isTablet);
 
         if(tabletSize){
-            textViewTigger.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+            textViewTrigger.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
             textViewTimeBase.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
         }
 
@@ -128,11 +133,12 @@ public class TimebaseTiggerFragment extends Fragment {
         });
         seekBarTimebase.setProgress(0);
 
-        seekBarTigger.setters(-16.5, 16.5);
-        seekBarTigger.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+        seekBarTrigger.setters(-16.5, 16.5);
+        seekBarTrigger.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                textViewTigger.setText(seekBarTigger.getValue() + " V");
+                textViewTrigger.setText(seekBarTrigger.getValue() + " V");
+                ((OscilloscopeActivity)getActivity()).trigger = seekBarTrigger.getValue();
             }
 
             @Override
@@ -145,7 +151,7 @@ public class TimebaseTiggerFragment extends Fragment {
 
             }
         });
-        seekBarTigger.setProgress(50);
+        seekBarTrigger.setProgress(50);
 
         String [] channels = {"CH1", "CH2", "CH3", "MIC"};
         ArrayAdapter<String> channelsAdapter;
@@ -163,12 +169,19 @@ public class TimebaseTiggerFragment extends Fragment {
         spinnerTiggerChannelSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                ((OscilloscopeActivity)getActivity()).tiggerChannel = spinnerTiggerChannelSelect.getItemAtPosition(position).toString();
+                ((OscilloscopeActivity)getActivity()).triggerChannel = spinnerTiggerChannelSelect.getItemAtPosition(position).toString();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        checkBoxTrigger.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                ((OscilloscopeActivity)getActivity()).isTriggerSelected = isChecked;
             }
         });
 
