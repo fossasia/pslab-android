cc77f2e76b94a675a3b1c22e4fd5c203f2a65ed1
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-07-12 20:26:57 +0530

    perform Experiment UI done

--- app/src/main/java/org/fossasia/pslab/activity/PerformExperimentActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/PerformExperimentActivity.java
@@ -2,11 +2,15 @@ package org.fossasia.pslab.activity;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
+import com.gigamole.navigationtabstrip.NavigationTabStrip;
+
 import org.fossasia.pslab.R;
+import org.fossasia.pslab.adapters.PerformExperimentAdapter;
 
 /**
  * Created by viveksb007 on 10/7/17.
@@ -29,6 +33,10 @@ public class PerformExperimentActivity extends AppCompatActivity {
             getSupportActionBar().setHomeButtonEnabled(true);
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        ViewPager viewPager = (ViewPager) findViewById(R.id.saved_experiment_vpager);
+        viewPager.setAdapter(new PerformExperimentAdapter(getSupportFragmentManager()));
+        NavigationTabStrip tabStrip = (NavigationTabStrip) findViewById(R.id.perform_experiment_tab_strip);
+        tabStrip.setViewPager(viewPager);
     }
 
     @Override
