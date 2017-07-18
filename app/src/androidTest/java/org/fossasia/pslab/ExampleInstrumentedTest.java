4c0001b84275aeecd0d330e93f13400d336c90b3
Vivek Singh <viveksbhadauria007@gmail.com>
2017-07-18 23:19:42 +0530

    added UI test to check all fragments (#335)

--- app/src/androidTest/java/org/fossasia/pslab/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/org/fossasia/pslab/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.viveksb007.pslab", appContext.getPackageName());
+        assertEquals("org.fossasia.pslab", appContext.getPackageName());
     }
 }
