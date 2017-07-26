48047171650a78d47b37e006a1fdd3bf981def9d
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-07-26 11:12:41 +0530

    Creates control dock for Sensor Activity
    Reformatted code

--- app/src/main/java/org/fossasia/pslab/activity/SensorActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/SensorActivity.java
@@ -5,6 +5,13 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import org.fossasia.pslab.R;
 
@@ -15,11 +22,41 @@ import org.fossasia.pslab.sensorfragment.SensorFragmentMain;
  */
 
 public class SensorActivity extends AppCompatActivity {
+    private RelativeLayout sensorDock;
+    private CheckBox indefiniteSamplesCheckBox;
+    private EditText samplesEditBox;
+    private SeekBar timegapSeekbar;
+    private TextView timegapLabel;
+    private ImageButton playPauseButton;
+    private boolean play;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sensor);
+
+        sensorDock = (RelativeLayout) findViewById(R.id.sensor_control_dock_layout);
+        indefiniteSamplesCheckBox = (CheckBox) findViewById(R.id.checkBox_samples_sensor);
+        samplesEditBox = (EditText) findViewById(R.id.editBox_samples_sensors);
+        timegapSeekbar = (SeekBar) findViewById(R.id.seekBar_timegap_sensor);
+        timegapLabel = (TextView) findViewById(R.id.tv_timegap_label);
+        playPauseButton = (ImageButton) findViewById(R.id.imageButton_play_pause_sensor);
+        play = false;
+
+
+        sensorDock.setVisibility(View.INVISIBLE);
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (play) {
+                    playPauseButton.setImageResource(R.drawable.play);
+                    play = false;
+                } else {
+                    playPauseButton.setImageResource(R.drawable.pause);
+                    play = true;
+                }
+            }
+        });
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction = fragmentManager.beginTransaction();
