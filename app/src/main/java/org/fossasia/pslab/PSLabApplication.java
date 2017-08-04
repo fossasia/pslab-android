3de7e98efa0e20172c6ab1fb9047b294349f3e34
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-08-05 00:26:25 +0530

    renamed Application class

--- app/src/main/java/org/fossasia/pslab/PSLabApplication.java
+++ app/src/main/java/org/fossasia/pslab/PSLabApplication.java
@@ -8,7 +8,7 @@ import io.realm.Realm;
  * Created by viveksb007 on 4/8/17.
  */
 
-public class MyApplication extends Application {
+public class PSLabApplication extends Application {
 
     @Override
     public void onCreate() {
