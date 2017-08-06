c7ab27407898319fabc3780851a47025add9b6f8
Asitava Sarkar <asitavasarkar@gmail.com>
2017-08-06 17:07:52 +0530

    Creates activity for Wireless Sensors

---
+++ app/src/main/java/org/fossasia/pslab/activity/WirelessSensorActivity.java
@@ -0,0 +1,17 @@
+package org.fossasia.pslab.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import org.fossasia.pslab.R;
+
+
+public class WirelessSensorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wireless_sensor);
+    }
+}
