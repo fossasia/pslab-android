7c6285bc7a3320b7ed1727f94fd9861a013981e0
Padmal <CloudyPadmal@users.noreply.github.com>
2017-08-14 14:41:26 +0800

    Fixes #498 : Hardcoded strings in saved experiments (#504)
    * Fixes #498 : Hardcoded strings in saved experiments
    
    * Update PerformExperimentAdapter.java

--- app/src/main/java/org/fossasia/pslab/adapters/PerformExperimentAdapter.java
+++ app/src/main/java/org/fossasia/pslab/adapters/PerformExperimentAdapter.java
@@ -89,7 +89,7 @@ public class PerformExperimentAdapter extends FragmentPagerAdapter {
                     return ExperimentDocFragment.newInstance("DUST_SENSOR.html");
                 if (experimentTitle.equals(context.getResources().getString(R.string.lemon_cell)))
                     return ExperimentDocFragment.newInstance("B_LEMON_CELL.html");
-                if (experimentTitle.equals("Half Wave Rectifier"))
+                if (experimentTitle.equals(context.getString(R.string.half_wave_rectifier)))
                     return  ExperimentDocFragment.newInstance("L_halfWave.html");
                 if (experimentTitle.equals(context.getResources().getString(R.string.full_wave_rectifier)))
                     return ExperimentDocFragment.newInstance("M_FullWave.html");
@@ -120,14 +120,14 @@ public class PerformExperimentAdapter extends FragmentPagerAdapter {
                     return TransistorAmplifierExperiment.newInstance();
                 if (experimentTitle.equals(context.getString(R.string.nfet_output_characteristics)))
                     return NFETOutputCharacteristicsExperiment.newInstance();
-                if (experimentTitle.equals("Half Wave Rectifier"))
-                    return RectifierExperiment.newInstance("Half Wave Rectifier");
+                if (experimentTitle.equals(context.getString(R.string.half_wave_rectifier)))
+                    return RectifierExperiment.newInstance(context.getString(R.string.half_wave_rectifier));
                 if (experimentTitle.equals(context.getString(R.string.lemon_cell)))
                     return LemonCellExperiment.newInstance();
                 if (experimentTitle.equals(context.getString(R.string.capacitor_discharge)))
                     return CapacitorDischargeExperiment.newInstance();
                 if (experimentTitle.equals(context.getResources().getString(R.string.full_wave_rectifier)))
-                    return RectifierExperiment.newInstance("Full Wave Rectifier");
+                    return RectifierExperiment.newInstance(context.getString(R.string.full_wave_rectifier));
                 return ExperimentSetupFragment.newInstance();
             default:
                 return ExperimentDocFragment.newInstance("astable-multivibrator.html");
