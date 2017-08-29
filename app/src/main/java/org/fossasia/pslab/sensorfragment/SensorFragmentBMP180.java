afb708b3a5a90758a09baf47cd6d956a8fc932f3
Akarshan Gandotra <akarshan.gandotra@gmail.com>
2017-08-29 18:22:16 +0530

    Added functionality to remaining Sensor fragments (#595)
    * Adds functionality to remaining sensor fragments
    
    Fixes #339

--- app/src/main/java/org/fossasia/pslab/sensorfragment/SensorFragmentBMP180.java
+++ app/src/main/java/org/fossasia/pslab/sensorfragment/SensorFragmentBMP180.java
@@ -19,6 +19,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import org.fossasia.pslab.R;
+import org.fossasia.pslab.activity.SensorActivity;
 import org.fossasia.pslab.communication.ScienceLab;
 import org.fossasia.pslab.communication.peripherals.I2C;
 import org.fossasia.pslab.communication.sensors.BMP180;
@@ -26,6 +27,8 @@ import org.fossasia.pslab.others.ScienceLabCommon;
 
 import java.io.IOException;
 import java.util.ArrayList;
+
+import static org.fossasia.pslab.activity.SensorActivity.counter;
 
 /**
  * Created by asitava on 10/7/17.
@@ -58,6 +61,7 @@ public class SensorFragmentBMP180 extends Fragment {
         super.onCreate(savedInstanceState);
         scienceLab = ScienceLabCommon.scienceLab;
         I2C i2c = scienceLab.i2c;
+        ((SensorActivity) getActivity()).sensorDock.setVisibility(View.VISIBLE);
         try {
             sensorBMP180 = new BMP180(i2c);
         } catch (IOException | InterruptedException e) {
@@ -67,7 +71,7 @@ public class SensorFragmentBMP180 extends Fragment {
             @Override
             public void run() {
                 while (true) {
-                    if (scienceLab.isConnected()) {
+                    if (scienceLab.isConnected() && ((SensorActivity) getActivity()).shouldPlay()) {
                         sensorDataFetch = new SensorDataFetch();
                         sensorDataFetch.execute();
 
@@ -84,7 +88,7 @@ public class SensorFragmentBMP180 extends Fragment {
                             }
                         }
                         try {
-                            Thread.sleep(500);
+                            Thread.sleep(((SensorActivity) getActivity()).timeGap);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -273,7 +277,11 @@ public class SensorFragmentBMP180 extends Fragment {
             mChartPressure.setVisibleXRangeMaximum(10);
             mChartPressure.moveViewToX(data.getEntryCount());
             mChartPressure.invalidate();
-
+            ((SensorActivity) getActivity()).samplesEditBox.setText(String.valueOf(counter));
+            if (counter == 0 && !((SensorActivity) getActivity()).runIndefinitely) {
+                ((SensorActivity) getActivity()).play = false;
+                ((SensorActivity) getActivity()).playPauseButton.setImageResource(R.drawable.play);
+            }
             synchronized (lock) {
                 lock.notify();
             }
