956680717b8fcff777bb882da1fec1b2d9050866
ThisIsNSH <hadanis.singh@gmail.com>
2018-03-30 15:51:04 +0530

    setting option added

--- app/src/main/java/org/fossasia/pslab/activity/MainActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/MainActivity.java
@@ -21,6 +21,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
@@ -138,6 +140,32 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_setting, menu);
+        MenuItem item = menu.findItem(R.id.action_settings);
+        if(navItemIndex==4)
+            item.setVisible(false);
+        else
+            item.setVisible(true);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // action with ID action_settings was selected
+            case R.id.action_settings:
+                navItemIndex = 4;
+                CURRENT_TAG = TAG_SETTINGS;
+                loadHomeFragment();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
     private void loadHomeFragment() {
         selectNavMenu();
         setToolbarTitle();
@@ -214,10 +242,10 @@ public class MainActivity extends AppCompatActivity {
                         navItemIndex = 3;
                         CURRENT_TAG = TAG_DESIGN_EXPERIMENTS;
                         break;
-                    case R.id.nav_settings:
+                   /* case R.id.nav_settings:
                         navItemIndex = 4;
                         CURRENT_TAG = TAG_SETTINGS;
-                        break;
+                        break;*/
                     case R.id.nav_about_us:
                         startActivity(new Intent(MainActivity.this, AboutUs.class));
                         if (drawer != null) {
