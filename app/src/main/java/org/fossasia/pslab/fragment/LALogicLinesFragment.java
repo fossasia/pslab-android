9b326d57d8516bcdc2c0be3171f598791489c63b
Rimjhim Bhadani <rimjhimbhadani@gmail.com>
2018-04-19 18:53:56 +0530

    Fixes #796 Removing unused import statements (#798)

--- app/src/main/java/org/fossasia/pslab/fragment/LALogicLinesFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/LALogicLinesFragment.java
@@ -11,7 +11,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
--- app/src/main/java/org/fossasia/pslab/fragment/TimebaseTriggerFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/TimebaseTriggerFragment.java
@@ -1,7 +1,5 @@
 package org.fossasia.pslab.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.TypedValue;
