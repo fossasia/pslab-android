a403f388ac38411faec9a65d62e14bd39a8248ac
Avjeet <singh.avjeet02@gmail.com>
2018-05-27 02:32:26 +0530

    Added UI for wavegen activity
    changed color and positioning of textview and increased font size
    
    changed design of seek bar
    
    Added controlling panel made changes in layout
    
    Unwanted items deleted
    
    code refractored to avoid conflicts
    
    some minor changes
    
    fixed seekbar version in config file

---
+++ app/src/main/java/org/fossasia/pslab/activity/WaveGeneratorActivity.java
@@ -0,0 +1,15 @@
+package org.fossasia.pslab.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import org.fossasia.pslab.R;
+
+public class WaveGeneratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wave_generator);
+    }
+}
--- app/src/main/java/org/fossasia/pslab/fragment/InstrumentsFragment.java
+++ app/src/main/java/org/fossasia/pslab/fragment/InstrumentsFragment.java
@@ -22,6 +22,7 @@ import org.fossasia.pslab.activity.MultimeterActivity;
 import org.fossasia.pslab.activity.OscilloscopeActivity;
 import org.fossasia.pslab.activity.PowerSourceActivity;
 import org.fossasia.pslab.activity.SensorActivity;
+import org.fossasia.pslab.activity.WaveGeneratorActivity;
 import org.fossasia.pslab.items.ApplicationItem;
 import org.fossasia.pslab.R;
 import org.fossasia.pslab.adapters.ApplicationAdapter;
@@ -75,7 +76,8 @@ public class InstrumentsFragment extends Fragment {
                                 startActivity(intent);
                                 break;
                             case "Wave Generator":
-                                Toast.makeText(getContext(), getResources().getString(R.string.coming_soon), Toast.LENGTH_SHORT).show();
+                                intent = new Intent(context, WaveGeneratorActivity.class);
+                                startActivity(intent);
                                 break;
                             case "Power Source":
                                 intent = new Intent(context, PowerSourceActivity.class);
