401a2f48c1e3d3390f3dee7b7bfaac531e7f8bb8
Areeb Jamal <jamal.areeb@gmail.com>
2018-06-03 21:28:13 +0530

    chore: Remove apk before zipaligning (#963)

--- upload-apk.sh
+++ upload-apk.sh
@@ -23,6 +23,7 @@ if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "Push to master branch detected, signing the app..."
     cp app-release-unsigned.apk app-release-unaligned.apk
 	jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
+	\rm -f app-release.apk
 	${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
 fi
 
