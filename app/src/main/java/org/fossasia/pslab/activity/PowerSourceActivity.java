05f1c7095bea98573f0a9f3143a4a96f979d0895
Padmal <CloudyPadmal@users.noreply.github.com>
2018-06-07 06:06:44 +0530

    feat: layout for power source instrument (#960)

--- app/src/main/java/org/fossasia/pslab/activity/PowerSourceActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/PowerSourceActivity.java
@@ -2,11 +2,15 @@ package org.fossasia.pslab.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import com.sdsmdg.harjot.crollerTest.Croller;
+import com.sdsmdg.harjot.crollerTest.OnCrollerChangeListener;
 
 import org.fossasia.pslab.R;
-import org.fossasia.pslab.adapters.ControlMainAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Abhinav Raj on 1/6/18.
@@ -14,17 +18,47 @@ import org.fossasia.pslab.adapters.ControlMainAdapter;
 
 public class PowerSourceActivity extends AppCompatActivity {
 
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.power_card_pv1_controller)
+    Croller controllerPV1;
+    @BindView(R.id.power_card_pv2_controller)
+    Croller controllerPV2;
+    @BindView(R.id.power_card_pv3_controller)
+    Croller controllerPV3;
+    @BindView(R.id.power_card_pcs_controller)
+    Croller controllerPCS;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_power_source);
 
-        ControlMainAdapter mAdapter = new ControlMainAdapter(new String[]{"PV1", "PV2", "PV3", "PCS", "WAVE 1", "WAVE 2", "SQUARE"});
-        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.control_main_recycler_view);
-        mRecyclerView.setHasFixedSize(false);
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLayoutManager);
-        mRecyclerView.setAdapter(mAdapter);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+
+        monitorControllers(controllerPV1);
+        monitorControllers(controllerPV2);
+        monitorControllers(controllerPV3);
+        monitorControllers(controllerPCS);
+    }
+
+    private void monitorControllers(Croller controller) {
+        controller.setOnCrollerChangeListener(new OnCrollerChangeListener() {
+            @Override
+            public void onProgressChanged(Croller croller, int progress) {
+                /* TODO: Use progress to output desired voltage */
+            }
+
+            @Override
+            public void onStartTrackingTouch(Croller croller) {/**/}
+
+            @Override
+            public void onStopTrackingTouch(Croller croller) {
+                /* TODO: Output desired voltage when this method is called */
+            }
+        });
     }
 
     @Override
--- app/src/main/java/org/fossasia/pslab/items/SquareImageButton.java
+++ app/src/main/java/org/fossasia/pslab/items/SquareImageButton.java
@@ -1,23 +1,26 @@
 package org.fossasia.pslab.items;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.widget.ImageView;
+import android.widget.ImageButton;
+
 
 /**
  * Created by Padmal on 5/8/17.
  */
 
-public class SquareImageView extends ImageView {
-    public SquareImageView(Context context) {
+@SuppressLint("AppCompatCustomView")
+public class SquareImageButton extends ImageButton {
+    public SquareImageButton(Context context) {
         super(context);
     }
 
-    public SquareImageView(Context context, AttributeSet attrs) {
+    public SquareImageButton(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public SquareImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SquareImageButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
