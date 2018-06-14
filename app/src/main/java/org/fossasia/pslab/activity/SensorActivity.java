249a60970a46c00f60ecb841d8f8df932b4c2abb
harsh-2711 <harshaudi27@gmail.com>
2018-06-14 18:19:49 +0530

    Changed fragments to activities for each sensor and fixed bugs of changing orientation and pressing back key

--- app/src/main/java/org/fossasia/pslab/activity/SensorActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/SensorActivity.java
@@ -1,27 +1,38 @@
 package org.fossasia.pslab.activity;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.annotation.Nullable;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.RelativeLayout;
-import android.widget.SeekBar;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.fossasia.pslab.R;
+import org.fossasia.pslab.communication.ScienceLab;
+import org.fossasia.pslab.communication.peripherals.I2C;
+import org.fossasia.pslab.others.ScienceLabCommon;
+import org.fossasia.pslab.sensors.SensorADS1115;
+import org.fossasia.pslab.sensors.SensorBMP180;
+import org.fossasia.pslab.sensors.SensorHMC5883L;
+import org.fossasia.pslab.sensors.SensorMLX90614;
+import org.fossasia.pslab.sensors.SensorMPU6050;
+import org.fossasia.pslab.sensors.SensorMPU925X;
+import org.fossasia.pslab.sensors.SensorSHT21;
+import org.fossasia.pslab.sensors.SensorTSL2561;
 
-import org.fossasia.pslab.sensorfragment.SensorFragmentMain;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  * Created by asitava on 18/6/17.
@@ -29,112 +40,151 @@ import butterknife.ButterKnife;
 
 public class SensorActivity extends AppCompatActivity {
 
-    @BindView(R.id.sensor_control_dock_layout)
-    public RelativeLayout sensorDock;
-    @BindView(R.id.checkBox_samples_sensor)
-    public CheckBox indefiniteSamplesCheckBox;
-    @BindView(R.id.editBox_samples_sensors)
-    public EditText samplesEditBox;
-    @BindView(R.id.seekBar_timegap_sensor)
-    SeekBar timeGapSeekbar;
-    @BindView(R.id.tv_timegap_label)
-    TextView timeGapLabel;
-    @BindView(R.id.imageButton_play_pause_sensor)
-    public ImageButton playPauseButton;
-    public boolean play;
-    public boolean runIndefinitely;
-    public static int counter;
-    public int timeGap;
+    private I2C i2c;
+    private ScienceLab scienceLab;
+    private LinkedHashMap<Integer, String> sensorAddr = new LinkedHashMap<>();
+    private ArrayList<String> dataAddress = new ArrayList<>();
+    private ArrayList<String> dataName = new ArrayList<>();
+    private ArrayAdapter<String> adapter;
+    private String tvData = "";
+    private ListView lvSensor;
+    private TextView tvSensorScan;
+
+    Runnable scanRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (scienceLab.isConnected()) {
+                populateSensors();
+            }
+        }
+    };
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor);
-        ButterKnife.bind(this);
+        setContentView(R.layout.sensor_main);
+        scienceLab = ScienceLabCommon.scienceLab;
 
-        play = false;
-        runIndefinitely = true;
-        timeGap = 100;
-        final int step = 1;
-        final int max = 1000;
-        final int min = 100;
+        i2c = scienceLab.i2c;
+        sensorAddr.put(0x60, "MCP4728");
+        sensorAddr.put(0x48, "ADS1115");
+        sensorAddr.put(0x23, "BH1750");
+        sensorAddr.put(0x77, "BMP180");
+        sensorAddr.put(0x5A, "MLX90614");
+        sensorAddr.put(0x1E, "HMC5883L");
+        sensorAddr.put(0x68, "MPU6050");
+        sensorAddr.put(0x40, "SHT21");
+        sensorAddr.put(0x39, "TSL2561");
 
-        playPauseButton.setOnClickListener(new View.OnClickListener() {
+        adapter = new ArrayAdapter<>(getApplication(), R.layout.sensor_list_item, R.id.tv_sensor_list_item, dataName);
+
+        final CoordinatorLayout coordinatorLayout = findViewById(R.id.layout_container);
+        Button buttonSensorAutoScan = findViewById(R.id.button_sensor_autoscan);
+        tvSensorScan = findViewById(R.id.tv_sensor_scan);
+        tvSensorScan.setText(tvData);
+        lvSensor = findViewById(R.id.lv_sensor);
+        lvSensor.setAdapter(adapter);
+
+        buttonSensorAutoScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (play) {
-                    playPauseButton.setImageResource(R.drawable.play);
-                    play = false;
+                if (scienceLab.isConnected()) {
+                    new Thread(scanRunnable).start();
                 } else {
-                    playPauseButton.setImageResource(R.drawable.pause);
-                    play = true;
-                    if (!indefiniteSamplesCheckBox.isChecked()) {
-                        counter = Integer.parseInt(samplesEditBox.getText().toString());
-                    }
+                    Snackbar snackbar = Snackbar.make(coordinatorLayout, getResources().getString(R.string.device_not_connected), Snackbar.LENGTH_SHORT);
+                    View snackBarView = snackbar.getView();
+                    TextView snackBarTextView = snackBarView.findViewById(android.support.design.R.id.snackbar_text);
+                    snackBarTextView.setTextColor(Color.YELLOW);
+                    snackbar.show();
                 }
             }
         });
-        sensorDock.setVisibility(View.GONE);
-
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.sensor_layout, SensorFragmentMain.newInstance());
-        transaction.commit();
-
-        indefiniteSamplesCheckBox.setChecked(true);
-        samplesEditBox.setEnabled(false);
-        indefiniteSamplesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        lvSensor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    runIndefinitely = true;
-                    samplesEditBox.setEnabled(false);
-                }
-                else {
-                    runIndefinitely = false;
-                    samplesEditBox.setEnabled(true);
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String itemValue = (String) lvSensor.getItemAtPosition(position);
+                Intent intent;
+                switch (itemValue) {
+                    case "ADS1115":
+                        intent = new Intent(getApplication(), SensorADS1115.class);
+                        startActivity(intent);
+                        break;
+                    case "BMP180":
+                        intent = new Intent(getApplication(), SensorBMP180.class);
+                        startActivity(intent);
+                        break;
+                    case "MLX90614":
+                        intent = new Intent(getApplication(), SensorMLX90614.class);
+                        startActivity(intent);
+                        break;
+                    case "HMC5883L":
+                        intent = new Intent(getApplication(), SensorHMC5883L.class);
+                        startActivity(intent);
+                        break;
+                    case "MPU6050":
+                        intent = new Intent(getApplication(), SensorMPU6050.class);
+                        startActivity(intent);
+                        break;
+                    case "SHT21":
+                        intent = new Intent(getApplication(), SensorSHT21.class);
+                        startActivity(intent);
+                        break;
+                    case "TSL2561":
+                        intent = new Intent(getApplication(), SensorTSL2561.class);
+                        startActivity(intent);
+                        break;
+                    case "MPU925x":
+                        intent = new Intent(getApplication(), SensorMPU925X.class);
+                        startActivity(intent);
+                        break;
+                    default:
+                        Toast.makeText(getApplication(), "Sensor Not Supported", Toast.LENGTH_SHORT).show();
                 }
             }
         });
-
-        timeGapSeekbar.setMax( (max - min) / step );
-        timeGapSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                timeGap = min + (progress * step);
-                timeGapLabel.setText(timeGap + "ms");
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
+        new Thread(scanRunnable).start();
     }
 
-    public boolean shouldPlay() {
-        if (play) {
-            if (indefiniteSamplesCheckBox.isChecked())
-                return true;
-            else if (counter >= 0) {
-                counter--;
-                return true;
-            } else {
-                play = false;
-                return false;
-            }
-        } else {
-            return false;
+    private void populateSensors() {
+        ArrayList<Integer> data = new ArrayList<>();
+        dataName.clear();
+        dataAddress.clear();
+        try {
+            data = i2c.scan(null);
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
         }
-    }
+        if (data != null) {
+            for (Integer myInt : data) {
+                if (myInt != null && sensorAddr.get(myInt) != null) {
+                    dataAddress.add(String.valueOf(myInt));
+                }
+            }
+            tvData = "";
 
-    @Override
-    public void onBackPressed() {
-       finish();
+            for (String s : dataAddress) {
+                tvData += s + ":" + sensorAddr.get(Integer.parseInt(s)) + "\n";
+            }
+
+        }
+
+        for (int key : sensorAddr.keySet()) {
+            dataName.add(sensorAddr.get(key));
+        }
+
+        for (int key : sensorAddr.keySet()) {
+            dataName.add(sensorAddr.get(key));
+        }
+
+        for (int key : sensorAddr.keySet()) {
+            dataName.add(sensorAddr.get(key));
+        }
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                tvSensorScan.setText(tvData);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/org/fossasia/pslab/sensors/SensorADS1115.java
+++ app/src/main/java/org/fossasia/pslab/sensors/SensorADS1115.java
@@ -1,13 +1,17 @@
-package org.fossasia.pslab.sensorfragment;
+package org.fossasia.pslab.sensors;
 
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -20,7 +24,6 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 
 import org.fossasia.pslab.R;
-import org.fossasia.pslab.activity.SensorActivity;
 import org.fossasia.pslab.communication.ScienceLab;
 import org.fossasia.pslab.communication.peripherals.I2C;
 import org.fossasia.pslab.communication.sensors.ADS1115;
@@ -29,47 +32,61 @@ import org.fossasia.pslab.others.ScienceLabCommon;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import static org.fossasia.pslab.activity.SensorActivity.counter;
-
 /**
- * Created by asitava on 10/7/17.
+ * Created by Harsh on 6/6/18.
  */
 
-public class SensorFragmentADS1115 extends Fragment {
-
+public class SensorADS1115 extends AppCompatActivity {
+    private static int counter;
+    private final Object lock = new Object();
     private ScienceLab scienceLab;
-    private SensorDataFetch sensorDataFetch;
+    private SensorADS1115.SensorDataFetch sensorDataFetch;
     private TextView tvSensorADS1115;
     private LineChart mChart;
     private long startTime;
     private int flag;
     private ArrayList<Entry> entries;
-    private final Object lock = new Object();
+    private RelativeLayout sensorDock;
+    private CheckBox indefiniteSamplesCheckBox;
+    private EditText samplesEditBox;
+    private SeekBar timeGapSeekbar;
+    private TextView timeGapLabel;
+    private ImageButton playPauseButton;
+    private boolean play;
+    private boolean runIndefinitely;
+    private int timeGap;
 
     private ADS1115 sensorADS1115;
 
-    public static SensorFragmentADS1115 newInstance() {
-        return new SensorFragmentADS1115();
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.sensor_ads1115);
+
+        sensorDock = (RelativeLayout) findViewById(R.id.sensor_control_dock_layout);
+        indefiniteSamplesCheckBox = (CheckBox) findViewById(R.id.checkBox_samples_sensor);
+        samplesEditBox = (EditText) findViewById(R.id.editBox_samples_sensors);
+        timeGapSeekbar = (SeekBar) findViewById(R.id.seekBar_timegap_sensor);
+        timeGapLabel = (TextView) findViewById(R.id.tv_timegap_label);
+        playPauseButton = (ImageButton) findViewById(R.id.imageButton_play_pause_sensor);
+        setSensorDock();
+        sensorDock.setVisibility(View.VISIBLE);
+
         scienceLab = ScienceLabCommon.scienceLab;
         I2C i2c = scienceLab.i2c;
         entries = new ArrayList<>();
-        ((SensorActivity) getActivity()).sensorDock.setVisibility(View.VISIBLE);
         try {
             sensorADS1115 = new ADS1115(i2c);
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
+
         Runnable runnable = new Runnable() {
             @Override
             public void run() {
                 while (true) {
-                    if (scienceLab.isConnected() && ((SensorActivity) getActivity()).shouldPlay()) {
-                        sensorDataFetch = new SensorDataFetch();
+                    if (scienceLab.isConnected() && shouldPlay()) {
+                        sensorDataFetch = new SensorADS1115.SensorDataFetch();
                         sensorDataFetch.execute();
                         if (flag == 0) {
                             startTime = System.currentTimeMillis();
@@ -83,7 +100,7 @@ public class SensorFragmentADS1115 extends Fragment {
                             }
                         }
                         try {
-                            Thread.sleep(((SensorActivity) getActivity()).timeGap);
+                            Thread.sleep(timeGap);
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
@@ -92,18 +109,13 @@ public class SensorFragmentADS1115 extends Fragment {
             }
         };
         new Thread(runnable).start();
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.sensor_ads1115, container, false);
+        tvSensorADS1115 = findViewById(R.id.tv_sensor_ads1115);
+        mChart = findViewById(R.id.chart_sensor_ads);
 
-        tvSensorADS1115 = view.findViewById(R.id.tv_sensor_ads1115);
-        mChart = view.findViewById(R.id.chart_sensor_ads);
-
-        Spinner spinnerSensorADS1115Gain = view.findViewById(R.id.spinner_sensor_ads1115_gain);
-        Spinner spinnerSensorADS1115Channel = view.findViewById(R.id.spinner_sensor_ads1115_channel);
-        Spinner spinnerSensorADS1115Rate = view.findViewById(R.id.spinner_sensor_ads1115_rate);
+        Spinner spinnerSensorADS1115Gain = findViewById(R.id.spinner_sensor_ads1115_gain);
+        Spinner spinnerSensorADS1115Channel = findViewById(R.id.spinner_sensor_ads1115_channel);
+        Spinner spinnerSensorADS1115Rate = findViewById(R.id.spinner_sensor_ads1115_rate);
 
         if (sensorADS1115 != null) {
             sensorADS1115.setGain(spinnerSensorADS1115Gain.getSelectedItem().toString());
@@ -147,8 +159,82 @@ public class SensorFragmentADS1115 extends Fragment {
         y.setLabelCount(10);
 
         y2.setDrawGridLines(false);
+    }
 
-        return view;
+    private boolean shouldPlay() {
+        if (play) {
+            if (indefiniteSamplesCheckBox.isChecked())
+                return true;
+            else if (counter >= 0) {
+                counter--;
+                return true;
+            } else {
+                play = false;
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    private void setSensorDock() {
+        play = false;
+        runIndefinitely = true;
+        timeGap = 100;
+        final int step = 1;
+        final int max = 1000;
+        final int min = 100;
+
+        playPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (play) {
+                    playPauseButton.setImageResource(R.drawable.play);
+                    play = false;
+                } else {
+                    playPauseButton.setImageResource(R.drawable.pause);
+                    play = true;
+                    if (!indefiniteSamplesCheckBox.isChecked()) {
+                        counter = Integer.parseInt(samplesEditBox.getText().toString());
+                    }
+                }
+            }
+        });
+        sensorDock.setVisibility(View.VISIBLE);
+
+        indefiniteSamplesCheckBox.setChecked(true);
+        samplesEditBox.setEnabled(false);
+        indefiniteSamplesCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    runIndefinitely = true;
+                    samplesEditBox.setEnabled(false);
+                } else {
+                    runIndefinitely = false;
+                    samplesEditBox.setEnabled(true);
+                }
+            }
+        });
+
+        timeGapSeekbar.setMax((max - min) / step);
+        timeGapSeekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                timeGap = min + (progress * step);
+                timeGapLabel.setText(timeGap + "ms");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 
     private class SensorDataFetch extends AsyncTask<Void, Void, Void> {
@@ -184,10 +270,10 @@ public class SensorFragmentADS1115 extends Fragment {
             mChart.setVisibleXRangeMaximum(10);
             mChart.moveViewToX(data.getEntryCount());
             mChart.invalidate();
-            ((SensorActivity) getActivity()).samplesEditBox.setText(String.valueOf(counter));
-            if (counter == 0 && !((SensorActivity) getActivity()).runIndefinitely) {
-                ((SensorActivity) getActivity()).play = false;
-                ((SensorActivity) getActivity()).playPauseButton.setImageResource(R.drawable.play);
+            samplesEditBox.setText(String.valueOf(counter));
+            if (counter == 0 && !runIndefinitely) {
+                play = false;
+                playPauseButton.setImageResource(R.drawable.play);
             }
             synchronized (lock) {
                 lock.notify();
@@ -195,4 +281,3 @@ public class SensorFragmentADS1115 extends Fragment {
         }
     }
 }
-
