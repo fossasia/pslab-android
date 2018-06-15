5a5aaf618192bbc9ba73afe607fd6b16f753e1f0
Avjeet <singh.avjeet02@gmail.com>
2018-06-15 11:44:09 +0530

    Added working of PWM panel (#1010)
    default case added
    
    space removed

--- app/src/main/java/org/fossasia/pslab/others/WaveGeneratorCommon.java
+++ app/src/main/java/org/fossasia/pslab/others/WaveGeneratorCommon.java
@@ -8,17 +8,35 @@ public class WaveGeneratorCommon {
 
     public static HashMap<String, HashMap<String, Integer>> wave;
 
+
     public WaveGeneratorCommon() {
         wave = new HashMap<>();
 
         wave.put(WaveGeneratorActivity.WAVE1, new HashMap<String, Integer>());
         wave.put(WaveGeneratorActivity.WAVE2, new HashMap<String, Integer>());
 
-        wave.get(WaveGeneratorActivity.WAVE1).put(WaveGeneratorActivity.WAVETYPE, 1);
-        wave.get(WaveGeneratorActivity.WAVE1).put(WaveGeneratorActivity.FREQUENCY, 10);
+        wave.put(WaveGeneratorActivity.SQ1, new HashMap<String, Integer>());
+        wave.put(WaveGeneratorActivity.SQ2, new HashMap<String, Integer>());
+        wave.put(WaveGeneratorActivity.SQ3, new HashMap<String, Integer>());
+        wave.put(WaveGeneratorActivity.SQ4, new HashMap<String, Integer>());
 
-        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.WAVETYPE, 1);
-        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.PHASE, 0);
-        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.FREQUENCY, 10);
+        wave.get(WaveGeneratorActivity.WAVE1).put(WaveGeneratorActivity.FREQUENCY, WaveGeneratorActivity.FREQ_MIN);
+        wave.get(WaveGeneratorActivity.WAVE1).put(WaveGeneratorActivity.WAVETYPE, WaveGeneratorActivity.SIN);
+
+        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.PHASE, WaveGeneratorActivity.PHASE_MIN);
+        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.FREQUENCY, WaveGeneratorActivity.FREQ_MIN);
+        wave.get(WaveGeneratorActivity.WAVE2).put(WaveGeneratorActivity.WAVETYPE, WaveGeneratorActivity.SIN);
+
+        wave.get(WaveGeneratorActivity.SQ1).put(WaveGeneratorActivity.FREQUENCY, WaveGeneratorActivity.FREQ_MIN); //common frequency for all pins(SQ1,SQ2,SQ3,SQ4)
+        wave.get(WaveGeneratorActivity.SQ1).put(WaveGeneratorActivity.DUTY, WaveGeneratorActivity.DUTY_MIN);
+
+        wave.get(WaveGeneratorActivity.SQ2).put(WaveGeneratorActivity.PHASE, WaveGeneratorActivity.PHASE_MIN);
+        wave.get(WaveGeneratorActivity.SQ2).put(WaveGeneratorActivity.DUTY, WaveGeneratorActivity.DUTY_MIN);
+
+        wave.get(WaveGeneratorActivity.SQ3).put(WaveGeneratorActivity.PHASE, WaveGeneratorActivity.PHASE_MIN);
+        wave.get(WaveGeneratorActivity.SQ3).put(WaveGeneratorActivity.DUTY, WaveGeneratorActivity.DUTY_MIN);
+
+        wave.get(WaveGeneratorActivity.SQ4).put(WaveGeneratorActivity.PHASE, WaveGeneratorActivity.PHASE_MIN);
+        wave.get(WaveGeneratorActivity.SQ4).put(WaveGeneratorActivity.DUTY, WaveGeneratorActivity.DUTY_MIN);
     }
 }
