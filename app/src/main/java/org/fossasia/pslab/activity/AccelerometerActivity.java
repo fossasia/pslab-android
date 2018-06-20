fba623ae56779d1894e2a332e39178ff0ceeec8b
Vikum94 <vikum.14@cse.mrt.ac.lk>
2018-06-20 12:10:09 +0530

    Layout for the Accelerometer (#994)
    configure tab added
    
    new layout
    
    revert manifest file
    
    manifest file edits
    
    naming edits
    
    new recycler view layout
    
    fixes
    
    few changes
    
    color resource
    
    instrument icon and conflict fix
    
    min max value textboxes
    
    dimension updates
    
    removing unused resources
    
    removed yet another unused resource

---
+++ app/src/main/java/org/fossasia/pslab/activity/AccelerometerActivity.java
@@ -0,0 +1,24 @@
+package org.fossasia.pslab.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import org.fossasia.pslab.R;
+import org.fossasia.pslab.adapters.AccelerometerAdapter;
+
+public class AccelerometerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accelerometer);
+        AccelerometerAdapter adapter = new AccelerometerAdapter(new String[]{"X axis", "Y axis", "Z axis"});
+
+        RecyclerView recyclerView = this.findViewById(R.id.accelerometer_recycler_view);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
