af43e8dea9c509f9d23ff078cbbdcfc71e3b4b8e
Avjeet <singh.avjeet02@gmail.com>
2018-07-10 06:43:19 +0530

    included location in csv file (#1187)
    some minor change
    
    minor changes

--- app/src/main/java/org/fossasia/pslab/others/GPSLogger.java
+++ app/src/main/java/org/fossasia/pslab/others/GPSLogger.java
@@ -1,13 +1,23 @@
 package org.fossasia.pslab.others;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+
+import org.fossasia.pslab.R;
+import org.fossasia.pslab.activity.LuxMeterActivity;
 
 /**
  * Created by Padmal on 6/29/18.
@@ -15,31 +25,46 @@ import android.support.v4.app.ActivityCompat;
 
 public class GPSLogger {
 
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
     private LocationManager locationManager;
     private Context context;
-    private boolean locationAvailable = false;
+    public static boolean permissionAvailable = false;
+    private Location bestLocation;
 
     public GPSLogger(Context context, LocationManager locationManager) {
         this.context = context;
         this.locationManager = locationManager;
-        getUpdate();
+        callerActivity = (LuxMeterActivity) context;
     }
 
+    private LuxMeterActivity callerActivity;
     private LocationListener locationListener = new LocationListener() {
         @Override
         public void onLocationChanged(Location location) {
-            locationAvailable = true;
+            bestLocation = location;
         }
 
         @Override
         public void onStatusChanged(String s, int i, Bundle bundle) {/**/}
 
         @Override
-        public void onProviderEnabled(String s) {/**/}
+        public void onProviderEnabled(String s) { /**/}
 
         @Override
         public void onProviderDisabled(String s) {
-            // TODO: Handle GPS turned on/off situations
+            callerActivity.saveData = false;
+            new AlertDialog.Builder(callerActivity)
+                    .setTitle(R.string.allow_gps)
+                    .setMessage(R.string.allow_gps_info)
+                    .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                            context.startActivity(intent);
+                        }
+                    })
+                    .create()
+                    .show();
         }
     };
 
@@ -47,33 +72,48 @@ public class GPSLogger {
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
     }
 
+    @SuppressLint("MissingPermission")
     private void getUpdate() {
-        String provider = LocationManager.GPS_PROVIDER;
-        if (ActivityCompat.checkSelfPermission(context.getApplicationContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(context.getApplicationContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Handle location permissions
-            return;
+        if (permissionAvailable) {
+            String provider = LocationManager.GPS_PROVIDER;
+            locationManager.requestLocationUpdates(provider, 1000, 1,
+                    locationListener);
         }
-        locationManager.requestLocationUpdates(provider, 1000, 1,
-                locationListener);
     }
 
     public void removeUpdate() {
         locationManager.removeUpdates(locationListener);
     }
 
-    public void whatsLocation() {
-        if (ActivityCompat.checkSelfPermission(context.getApplicationContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                ActivityCompat.checkSelfPermission(context.getApplicationContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Handle location permissions
-            return;
+    @SuppressLint("MissingPermission")
+    public Location whatsLocation() {
+        if (permissionAvailable) {
+            return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         }
-        if (locationAvailable) {
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        return null;
+    }
+
+    public Location getBestLocation() {
+        return bestLocation;
+    }
+
+    public boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(context,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions((Activity) context,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_LOCATION);
+            return false;
+        } else {
+            permissionAvailable = true;
+            return true;
         }
+    }
+
+    public void startFetchingLocation() {
+        bestLocation = whatsLocation();
+        getUpdate();
     }
 }
