9c95746fea05dd66c65f0645393abc26aae2215f
Harsh Patel <harshaudi27@gmail.com>
2018-07-17 22:25:55 +0530

    Smoothed the data plot and changed some graph properties in Lux Meter instrument (#1234)

--- app/src/main/java/org/fossasia/pslab/fragment/LuxMeterFragmentData.java
+++ app/src/main/java/org/fossasia/pslab/fragment/LuxMeterFragmentData.java
@@ -31,6 +31,7 @@ import org.fossasia.pslab.activity.LuxMeterActivity;
 import org.fossasia.pslab.communication.ScienceLab;
 import org.fossasia.pslab.communication.peripherals.I2C;
 import org.fossasia.pslab.communication.sensors.BH1750;
+import org.fossasia.pslab.communication.sensors.TSL2561;
 import org.fossasia.pslab.others.CSVLogger;
 import org.fossasia.pslab.others.CustomSnackBar;
 import org.fossasia.pslab.others.GPSLogger;
@@ -38,9 +39,6 @@ import org.fossasia.pslab.others.ScienceLabCommon;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
-
-import org.fossasia.pslab.communication.sensors.TSL2561;
-
 import java.util.ArrayList;
 
 import butterknife.BindView;
@@ -52,10 +50,9 @@ import static android.content.Context.SENSOR_SERVICE;
 public class LuxMeterFragmentData extends Fragment {
 
     private static int sensorType = 0;
-    private static int highLimit = 1000;
+    private static int highLimit = 2000;
     private static int updatePeriod = 100;
-    private SensorDataFetch sensorDataFetch;
-
+    private final Object lock = new Object();
     public CSVLogger lux_logger;
 
     @BindView(R.id.lux_stat_max)
@@ -67,8 +64,9 @@ public class LuxMeterFragmentData extends Fragment {
     @BindView(R.id.chart_lux_meter)
     LineChart mChart;
     @BindView(R.id.light_meter)
-    PointerSpeedometer lightMeter;
 
+    PointerSpeedometer lightMeter;
+    private SensorDataFetch sensorDataFetch;
     private BH1750 sensorBH1750 = null;
     private TSL2561 sensorTSL2561 = null;
     private SensorManager sensorManager;
@@ -83,12 +81,17 @@ public class LuxMeterFragmentData extends Fragment {
     private volatile boolean monitor = true;
     private Unbinder unbinder;
     private boolean logged = false, writeHeader = false;
-
-    private final Object lock = new Object();
+    private long previousTimeElapsed = (System.currentTimeMillis() - startTime) / 1000;
     private GPSLogger gpsLogger;
 
     public static LuxMeterFragmentData newInstance() {
         return new LuxMeterFragmentData();
+    }
+
+    public static void setParameters(int sensorType, int highLimit, int updatePeriod) {
+        LuxMeterFragmentData.sensorType = sensorType;
+        LuxMeterFragmentData.highLimit = highLimit;
+        LuxMeterFragmentData.updatePeriod = updatePeriod;
     }
 
     @Override
@@ -228,10 +231,15 @@ public class LuxMeterFragmentData extends Fragment {
         return view;
     }
 
-    public static void setParameters(int sensorType, int highLimit, int updatePeriod) {
-        LuxMeterFragmentData.sensorType = sensorType;
-        LuxMeterFragmentData.highLimit = highLimit;
-        LuxMeterFragmentData.updatePeriod = updatePeriod;
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        monitor = false;
+        if (sensor != null && sensorDataFetch != null) {
+            sensorManager.unregisterListener(sensorDataFetch);
+            sensorDataFetch.cancel(true);
+        }
+        unbinder.unbind();
     }
 
     private class SensorDataFetch extends AsyncTask<Void, Void, Void> implements SensorEventListener {
@@ -253,7 +261,7 @@ public class LuxMeterFragmentData extends Fragment {
                     data = (float) dataSet[2];
                     sensorManager.unregisterListener(this);
                 } else if (sensor != null) {
-                    sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+                    sensorManager.registerListener(this, sensor, updatePeriod);
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -288,71 +296,81 @@ public class LuxMeterFragmentData extends Fragment {
             y.setAxisMaximum(currentMax);
             y.setAxisMinimum(currentMin);
             y.setLabelCount(10);
-            lightMeter.setSpeedAt(data);
+            if (data != 0) {
+                lightMeter.setSpeedAt(data);
 
-            if (data > highLimit) lightMeter.setPointerColor(Color.RED);
-            else lightMeter.setPointerColor(Color.WHITE);
-            timeElapsed = (System.currentTimeMillis() - startTime) / 1000;
-            entries.add(new Entry((float) timeElapsed, data));
-            final LuxMeterActivity parent = (LuxMeterActivity) getActivity();
-            for (Entry item : entries) {
-                assert parent != null;
-                if (parent.saveData) {
-                    if (!writeHeader) {
-                        gpsLogger = parent.gpsLogger;
-                        lux_logger = new CSVLogger(getString(R.string.lux_meter));
-                        lux_logger.writeCSVFile("Timestamp,X,Y\n");
-                        writeHeader = true;
-                    }
-                    String data = String.valueOf(System.currentTimeMillis()) + "," +
-                            item.getX() + "," + item.getY() + "\n";
-                    lux_logger.writeCSVFile(data);
-                    logged = true;
-                } else {
-                    if (logged) {
-                        writeHeader = false;
-                        logged = false;
-                        Location location = gpsLogger.getBestLocation();
-                        if (location != null) {
-                            String data = "\nLocation" + "," + String.valueOf(location.getLatitude()) + "," + String.valueOf(location.getLongitude() + "\n");
+                if (data > highLimit)
+                    lightMeter.setPointerColor(Color.RED);
+                else
+                    lightMeter.setPointerColor(Color.WHITE);
+
+                timeElapsed = (System.currentTimeMillis() - startTime) / 1000;
+                if (timeElapsed != previousTimeElapsed) {
+                    previousTimeElapsed = timeElapsed;
+                    entries.add(new Entry((float) timeElapsed, data));
+                    final LuxMeterActivity parent = (LuxMeterActivity) getActivity();
+                    for (Entry item : entries) {
+                        assert parent != null;
+                        if (parent.saveData) {
+                            if (!writeHeader) {
+                                gpsLogger = parent.gpsLogger;
+                                lux_logger = new CSVLogger(getString(R.string.lux_meter));
+                                lux_logger.writeCSVFile("Timestamp,X,Y\n");
+                                writeHeader = true;
+                            }
+                            String data = String.valueOf(System.currentTimeMillis()) + "," +
+                                    item.getX() + "," + item.getY() + "\n";
                             lux_logger.writeCSVFile(data);
+                            logged = true;
+                        } else {
+                            if (logged) {
+                                writeHeader = false;
+                                logged = false;
+                                Location location = gpsLogger.getBestLocation();
+                                if (location != null) {
+                                    String data = "\nLocation" + "," + String.valueOf(location.getLatitude()) + "," + String.valueOf(location.getLongitude() + "\n");
+                                    lux_logger.writeCSVFile(data);
+                                }
+                                gpsLogger.removeUpdate();
+                                CustomSnackBar.showSnackBar((CoordinatorLayout) parent.findViewById(R.id.cl),
+                                        getString(R.string.csv_store_text) + " " + lux_logger.getCurrentFilePath()
+                                        , getString(R.string.delete_capital), new View.OnClickListener() {
+                                            @Override
+                                            public void onClick(View view) {
+                                                new AlertDialog.Builder(parent, R.style.AlertDialogStyle)
+                                                        .setTitle(R.string.delete_file)
+                                                        .setMessage(R.string.delete_warning)
+                                                        .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                                                            @Override
+                                                            public void onClick(DialogInterface dialogInterface, int i) {
+                                                                lux_logger.deleteFile();
+                                                            }
+                                                        })
+                                                        .setNegativeButton(R.string.cancel, null)
+                                                        .create()
+                                                        .show();
+                                            }
+                                        });
+                            }
                         }
-                        gpsLogger.removeUpdate();
-                        CustomSnackBar.showSnackBar((CoordinatorLayout) parent.findViewById(R.id.cl),
-                                getString(R.string.csv_store_text) + " " + lux_logger.getCurrentFilePath()
-                                , getString(R.string.delete_capital), new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        new AlertDialog.Builder(parent, R.style.AlertDialogStyle)
-                                                .setTitle(R.string.delete_file)
-                                                .setMessage(R.string.delete_warning)
-                                                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
-                                                    @Override
-                                                    public void onClick(DialogInterface dialogInterface, int i) {
-                                                        lux_logger.deleteFile();
-                                                    }
-                                                })
-                                                .setNegativeButton(R.string.cancel, null)
-                                                .create()
-                                                .show();
-                                    }
-                                });
+                        count++;
+                        sum += item.getY();
                     }
+                    statMean.setText(Float.toString(Float.valueOf(df.format(sum / count))));
+
+                    LineDataSet dataSet = new LineDataSet(entries, getString(R.string.lux));
+                    LineData data = new LineData(dataSet);
+                    dataSet.setDrawCircles(false);
+                    dataSet.setDrawValues(false);
+                    dataSet.setLineWidth(2);
+
+                    mChart.setData(data);
+                    mChart.notifyDataSetChanged();
+                    mChart.setVisibleXRangeMaximum(20);
+                    mChart.moveViewToX(data.getEntryCount());
+                    mChart.invalidate();
                 }
-                count++;
-                sum += item.getY();
             }
-            statMean.setText(Float.toString(Float.valueOf(df.format(sum / count))));
-
-            LineDataSet dataSet = new LineDataSet(entries, getString(R.string.lux));
-            LineData data = new LineData(dataSet);
-            dataSet.setDrawCircles(false);
-
-            mChart.setData(data);
-            mChart.notifyDataSetChanged();
-            mChart.setVisibleXRangeMaximum(10);
-            mChart.moveViewToX(data.getEntryCount());
-            mChart.invalidate();
         }
 
         @Override
@@ -363,16 +381,5 @@ public class LuxMeterFragmentData extends Fragment {
         private void unRegisterListener() {
             sensorManager.unregisterListener(this);
         }
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        monitor = false;
-        if (sensor != null && sensorDataFetch != null) {
-            sensorManager.unregisterListener(sensorDataFetch);
-            sensorDataFetch.cancel(true);
-        }
-        unbinder.unbind();
     }
 }
