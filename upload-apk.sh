96a7093ff2cb8a3d27b8aa26f72064bb5cb8966e
Padmal <blog.padmal@gmail.com>
2018-07-19 08:40:22 +0530

    chore: build apks from development branch
    cp to mv

--- upload-apk.sh
+++ upload-apk.sh
@@ -2,6 +2,7 @@
 set -e
 
 export PUBLISH_BRANCH=${PUBLISH_BRANCH:-master}
+export DEVELOPMENT_BRANCH=${DEVELOPMENT_BRANCH:-development}
 
 #setup git
 git config --global user.email "noreply@travis.com"
@@ -21,10 +22,21 @@ cd apk
 
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     echo "Push to master branch detected, signing the app..."
+    # Retain apk files for testing
+    mv app-debug.apk app-master-debug.apk
+    # Sign APK
     cp app-release-unsigned.apk app-release-unaligned.apk
-	jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
-	\rm -f app-release.apk
-	${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
+    jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore ../scripts/key.jks -storepass $STORE_PASS -keypass $KEY_PASS app-release-unaligned.apk $ALIAS
+    \rm -f app-release.apk
+    ${ANDROID_HOME}/build-tools/27.0.3/zipalign -v -p 4 app-release-unaligned.apk app-release.apk
+    mv app-release-unsigned.apk app-master-unsigned.apk
+fi
+
+if [ "$TRAVIS_BRANCH" == "$DEVELOPMENT_BRANCH" ]; then
+    echo "Push to development branch detected, signing the app..."
+    # Retain apk files for testing
+    mv app-debug.apk app-dev-debug.apk
+    mv app-release-unsigned.apk app-dev-unsigned.apk
 fi
 
 git checkout --orphan workaround
@@ -47,7 +59,6 @@ fi
 
 # Publish App to Play Store
 if [ "$TRAVIS_BRANCH" != "$PUBLISH_BRANCH" ]; then
-    echo "We publish apk only for changes in master branch. So, let's skip this shall we ? :)"
     exit 0
 fi
 
