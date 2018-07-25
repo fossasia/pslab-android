780292ba3b778228336a68585e0947071809a5c0
Harsh Patel <harshaudi27@gmail.com>
2018-07-25 22:03:11 +0530

    Made a guide in Compass instrument to show how to calibrate the axis (#1281)
    Added bottomsheet guide for compass instrument
    
    Decreased the delay time for handler in compass instrument and removed unused code

--- app/src/main/java/org/fossasia/pslab/activity/CompassActivity.java
+++ app/src/main/java/org/fossasia/pslab/activity/CompassActivity.java
@@ -1,19 +1,32 @@
 package org.fossasia.pslab.activity;
 
+import android.content.SharedPreferences;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
 import org.fossasia.pslab.R;
+import org.fossasia.pslab.others.MathUtils;
+import org.fossasia.pslab.others.SwipeGestureDetector;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -23,6 +36,9 @@ import butterknife.ButterKnife;
  */
 
 public class CompassActivity extends AppCompatActivity implements SensorEventListener {
+
+    private static final String PREFS_NAME = "CompassPreference";
+
     @BindView(R.id.compass)
     ImageView compass;
     @BindView(R.id.degree_indicator)
@@ -42,6 +58,29 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     @BindView(R.id.tv_sensor_hmc5883l_bz)
     TextView zAxismagneticField;
 
+    @BindView(R.id.compass_toolbar)
+    Toolbar mToolbar;
+
+    @BindView(R.id.bottom_sheet)
+    LinearLayout bottomSheet;
+    @BindView(R.id.shadow)
+    View tvShadow;
+    @BindView(R.id.img_arrow)
+    ImageView arrowUpDown;
+    @BindView(R.id.sheet_slide_text)
+    TextView bottomSheetSlideText;
+    @BindView(R.id.guide_title)
+    TextView bottomSheetGuideTitle;
+    @BindView(R.id.custom_dialog_text)
+    TextView bottomSheetText;
+    @BindView(R.id.custom_dialog_schematic)
+    ImageView bottomSheetSchematic;
+    @BindView(R.id.custom_dialog_desc)
+    TextView bottomSheetDesc;
+
+    BottomSheetBehavior bottomSheetBehavior;
+    GestureDetector gestureDetector;
+    private SharedPreferences compassPreference;
     private float currentDegree = 0f;
     private int direction; // 0 for X-axis, 1 for Y-axis and 2 for Z-axis
     private SensorManager mSensorManager;
@@ -49,8 +88,13 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_compass);
+        setContentView(R.layout.activity_compass_main);
         ButterKnife.bind(this);
+
+        setSupportActionBar(mToolbar);
+
+        compassPreference = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        setUpBottomSheet();
 
         mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         xAxisRadioButton.setChecked(true);
@@ -152,4 +196,92 @@ public class CompassActivity extends AppCompatActivity implements SensorEventLis
         compass.startAnimation(ra);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_compass_help_menu, menu);
+        return true;
+    }
+
+    /**
+     * Initiates bottom sheet to display guide on how to use Compass instrument
+     */
+    private void setUpBottomSheet() {
+
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        boolean isFirstTime = compassPreference.getBoolean("CompassFirstTime", true);
+
+        if (isFirstTime) {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            tvShadow.setAlpha(0.8f);
+            arrowUpDown.setRotation(180);
+            bottomSheetSlideText.setText(R.string.hide_guide_text);
+            SharedPreferences.Editor editor = compassPreference.edit();
+            editor.putBoolean("CompassFirstTime", false);
+            editor.apply();
+        } else {
+            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        }
+
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            private Handler handler = new Handler();
+            private Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                    } catch (IllegalArgumentException e) {
+                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    }
+                }
+            };
+
+            @Override
+            public void onStateChanged(@NonNull final View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        handler.removeCallbacks(runnable);
+                        bottomSheetSlideText.setText(R.string.hide_guide_text);
+                        break;
+
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        handler.postDelayed(runnable, 1000);
+                        break;
+
+                    default:
+                        handler.removeCallbacks(runnable);
+                        bottomSheetSlideText.setText(R.string.show_guide_text);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                Float value = (float) MathUtils.map((double) slideOffset, 0.0, 1.0,
+                        0.0, 0.8);
+                tvShadow.setAlpha(value);
+                arrowUpDown.setRotation(slideOffset * 180);
+            }
+        });
+        gestureDetector = new GestureDetector(this,
+                new SwipeGestureDetector(bottomSheetBehavior));
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        gestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.compass_help_icon:
+                bottomSheetBehavior.setState(bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN ?
+                        BottomSheetBehavior.STATE_EXPANDED : BottomSheetBehavior.STATE_HIDDEN);
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
 }
