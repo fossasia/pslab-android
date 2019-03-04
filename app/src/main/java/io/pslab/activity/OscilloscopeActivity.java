09945e5673beeae6d7e9459524d57717fbff61f4
Riya Singh <43132209+riyasingh1004@users.noreply.github.com>
2019-03-04 22:55:16 +0530

    Feat:Press back button twice to exit oscilloscope

--- app/src/main/java/io/pslab/activity/OscilloscopeActivity.java
+++ app/src/main/java/io/pslab/activity/OscilloscopeActivity.java
@@ -27,6 +27,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
@@ -156,6 +157,7 @@ public class OscilloscopeActivity extends AppCompatActivity implements View.OnCl
     private volatile boolean monitor = true;
     private BottomSheetBehavior bottomSheetBehavior;
     private GestureDetector gestureDetector;
+    private boolean doubleBackToExitPressedOnce = false;
 
     private enum CHANNEL {CH1, CH2, CH3, MIC}
 
@@ -481,8 +483,22 @@ public class OscilloscopeActivity extends AppCompatActivity implements View.OnCl
 
     @Override
     public void onBackPressed() {
-        finish();
+       if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Press again to exit", Toast.LENGTH_SHORT)
+                .show();
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
+    
 
     @Override
     protected void onDestroy() {
