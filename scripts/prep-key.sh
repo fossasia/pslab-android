514f1ad6162373005bd76bccd6ae37fee091cf04
Padmal <CloudyPadmal@users.noreply.github.com>
2019-08-01 17:48:02 +0530

    chore: updated prep key script

--- scripts/prep-key.sh
+++ scripts/prep-key.sh
@@ -2,9 +2,8 @@
 set -e
 
 export DEPLOY_BRANCH=${DEPLOY_BRANCH:-master}
-export DEV_BRANCH=${DEV_BRANCH:-development}
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/pslab-android" ] || ! [ "$TRAVIS_BRANCH" == "$DEPLOY_BRANCH" -o "$TRAVIS_BRANCH" == "$DEV_BRANCH" ]; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_REPO_SLUG" != "fossasia/pslab-android" -o "$TRAVIS_BRANCH" != "$DEPLOY_BRANCH" ]; then
     echo "We decrypt key only for pushes to the master branch and not PRs. So, skip."
     exit 0
 fi
