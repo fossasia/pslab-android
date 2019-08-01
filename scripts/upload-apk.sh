663b7c4c2f7f909326c9582fcbef0fbf34e963fc
Padmal <CloudyPadmal@users.noreply.github.com>
2019-08-01 15:59:11 +0530

    chore: updating upload script
    experimental commit

--- scripts/upload-apk.sh
+++ scripts/upload-apk.sh
@@ -16,7 +16,7 @@ git clone --quiet --branch=apk https://fossasia:$GITHUB_API_KEY@github.com/fossa
 cd apk
 
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
-    rm -rf *
+    rm -rf pslab-master*
 else 
     rm -rf pslab-dev*
 fi
@@ -25,32 +25,37 @@ find ../app/build/outputs -type f \( -name "*.aab" -o -name "*.apk" \) -exec cp 
     
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
 
-    echo "Push to master branch detected, signing the app..."
+    echo "Push to master branch detected, generating apk..."
     # Retain apk files for testing
-    mv app-playstore-debug.apk pslab-master-debug.apk
-    mv app-fdroid-debug.apk pslab-master-debug-fdroid.apk
+    mv app-playstore-debug.apk pslab-master-playstore-debug.apk
+    mv app-fdroid-debug.apk pslab-master-fdroid-debug.apk
     # Generate temporary apk for signing
-    mv app-playstore-release.apk pslab-master-release.apk
-    mv app-fdroid-release.apk pslab-master-release-fdroid.apk
-    mv app.aab pslab-master-app.aab
+    mv app-playstore-release.apk pslab-master-playstore-release.apk
+    mv app-fdroid-release.apk pslab-master-fdroid-release.apk
+    mv app.aab pslab-master-app-bundle.aab
 fi
 
 if [ "$TRAVIS_BRANCH" == "$DEVELOPMENT_BRANCH" ]; then
 
     echo "Push to development branch detected, generating apk..."
     # Rename apks with dev prefixes
-    mv app-playstore-debug.apk pslab-dev-debug.apk
-    mv app-fdroid-debug.apk pslab-dev-debug-fdroid.apk
+    mv app-playstore-debug.apk pslab-dev-playstore-debug.apk
+    mv app-fdroid-debug.apk pslab-dev-fdroid-debug.apk
 
-    mv app-playstore-release.apk pslab-dev-release.apk
-    mv app-fdroid-release.apk pslab-dev-release-fdroid.apk
-    mv app.aab pslab-dev-app.aab
+    mv app-playstore-release.apk pslab-dev-playstore-release.apk
+    mv app-fdroid-release.apk pslab-dev-fdroid-release.apk
+    mv app.aab pslab-dev-app-bundle.aab
 fi
 
 git checkout --orphan temporary
 # Push generated apk files to apk branch
+# Debug printing TODO: Remove
+echo "Content in apk directory"
+ls
+echo "Content in one level up"
+ls ..
 git add .
-git commit -am "Travis build pushed [$TRAVIS_BRANCH]"
+git commit -am "Travis build pushed to [$TRAVIS_BRANCH]"
 
 # Delete current apk branch
 git branch -D apk
@@ -62,5 +67,5 @@ git push origin apk --force --quiet > /dev/null
 # Publish App to Play Store
 if [ "$TRAVIS_BRANCH" == "$PUBLISH_BRANCH" ]; then
     gem install fastlane
-    fastlane supply --aab pslab-master-app.aab --track alpha --json_key ../scripts/fastlane.json --package_name $PACKAGE_NAME
+    fastlane supply --aab pslab-master-app-bundle.aab --track alpha --json_key ../scripts/fastlane.json --package_name $PACKAGE_NAME
 fi
