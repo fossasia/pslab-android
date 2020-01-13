371dfce3356833dbd896726285f5a9a2547a568d
991rajat <991rajat@gmail.com>
2020-01-13 23:17:55 +0530

    feat: added input limit

--- app/src/main/java/io/pslab/InputMinMaxFilter.java
+++ app/src/main/java/io/pslab/InputMinMaxFilter.java
@@ -1,13 +1,12 @@
 package io.pslab;
 
-import android.content.Intent;
 import android.text.InputFilter;
 import android.text.Spanned;
-import android.util.Log;
 
 public class InputMinMaxFilter implements InputFilter {
 
-    private int min,max;
+    private int min;
+    private int max;
 
     public InputMinMaxFilter(int min, int max) {
         this.min = min;
@@ -25,16 +24,17 @@ public class InputMinMaxFilter implements InputFilter {
 
     @Override
     public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-        try{
-            String newVal = dest.toString().substring(0,dstart)+dest.toString().substring(dend,dest.toString().length());
+        try {
+            String newVal = dest.toString().substring(0, dstart) + dest.toString().substring(dend, dest.toString().length());
             newVal = newVal.substring(0, dstart) + source.toString() + newVal.substring(dstart, newVal.length());
             int input = Integer.parseInt(newVal);
-            if(newVal.length()>=4)
+            if (newVal.length() >= 4)
                 return "";
             if (isInRange(min, max, input))
                 return null;
-        } catch (NumberFormatException nfe) { }
-	    return "";
+        } catch (NumberFormatException nfe) {
+        }
+        return "";
 
     }
 }
