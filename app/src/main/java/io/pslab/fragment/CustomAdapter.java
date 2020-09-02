0871bda90b414ec8b94aeb7f2d96edc0fecb61a1
Souma <souma.hitech@gmail.com>
2020-09-02 19:08:51 +0530

    fix faq expandable list view error.

---
+++ app/src/main/java/io/pslab/fragment/CustomAdapter.java
@@ -0,0 +1,93 @@
+package io.pslab.fragment;
+
+import android.content.Context;
+import android.text.Html;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.List;
+
+import io.pslab.R;
+
+public class CustomAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private List<String> listHeader;
+    private HashMap<String,List<String>> listChild;
+    public CustomAdapter(Context context, List<String> listHeader, HashMap<String, List<String>> listChild) {
+        this.context = context;
+        this.listHeader = listHeader;
+        this.listChild = listChild;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listHeader.size();
+    }
+
+    @Override
+    public int getChildrenCount(int i) {
+        return listChild.get(listHeader.get(i)).size();
+    }
+
+    @Override
+    public Object getGroup(int i) {
+        return listHeader.get(i);
+    }
+
+    @Override
+    public Object getChild(int i, int i1) {
+        return listChild.get(listHeader.get(i)).get(i1);
+    }
+
+    @Override
+    public long getGroupId(int i) {
+        return i;
+    }
+
+    @Override
+    public long getChildId(int i, int i1) {
+        return i1;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int i, boolean b, View view, ViewGroup viewGroup) {
+        String question = (String) getGroup(i);
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_group,null);
+        }
+
+        TextView questionTextView = view.findViewById(R.id.question_textView);
+        questionTextView.setText(question);
+
+        return view;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        String answer = (String) getChild(i,i1);
+        if (view == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.list_item,null);
+        }
+
+        TextView answerTextView = view.findViewById(R.id.lblListItem);
+        answerTextView.setText(Html.fromHtml(answer));
+
+        return view;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
--- app/src/main/java/io/pslab/fragment/FAQFragment.java
+++ app/src/main/java/io/pslab/fragment/FAQFragment.java
@@ -1,22 +1,27 @@
 package io.pslab.fragment;
 
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.Html;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseExpandableListAdapter;
 import android.widget.ExpandableListView;
-import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import io.pslab.R;
 
 public class FAQFragment extends Fragment {
 
-    private String[] questions;
-    private String[][] answers;
+
+    private List<String> listHeader;
+    private HashMap<String,List<String>> listChild;
+    private ExpandableListView expandableListView;
+    private CustomAdapter customAdapter;
 
     public static FAQFragment newInstance() {
         return new FAQFragment();
@@ -25,128 +30,34 @@ public class FAQFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        questions = getResources().getStringArray(R.array.faq_questions);
-
-        String[] ans = getResources().getStringArray(R.array.faq_answers);
-        answers = new String[ans.length][];
-        for (int i = 0; i < ans.length; i++) {
-            answers[i] = new String[]{ans[i]};
-        }
     }
+
+    private void prepareListData() {
+        String[] allQuestion = getResources().getStringArray(R.array.faq_questions);
+        String[] allAnswer = getResources().getStringArray(R.array.faq_answers);
+
+        listHeader = new ArrayList<>();
+        listChild = new HashMap<>();
+
+        for (int i = 0; i < allQuestion.length; i++) {
+            listHeader.add(allQuestion[i]);
+            List<String> child = new ArrayList<>();
+            child.add(allAnswer[i]);
+            listChild.put(listHeader.get(i),child);
+        }
+
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView;
-        rootView = inflater.inflate(R.layout.fragment_faq, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_faq, container, false);
+        expandableListView = rootView.findViewById(R.id.expListView);
+        prepareListData();
+        customAdapter = new CustomAdapter(getContext(),listHeader,listChild);
+        expandableListView.setAdapter(customAdapter);
         return rootView;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        ExpandableListView listView;
 
-        listView = (ExpandableListView) view.findViewById(R.id.expListView);
-        listView.setAdapter(new ExpandableListAdapter(questions, answers));
-        listView.setGroupIndicator(null);
-
-    }
-
-    public class ExpandableListAdapter extends BaseExpandableListAdapter {
-
-        private final LayoutInflater inf;
-        private String[] questions;
-        private String[][] answers;
-
-        public ExpandableListAdapter(String[] questions, String[][] answers) {
-            this.questions = questions;
-            this.answers = answers;
-            inf = LayoutInflater.from(getActivity());
-        }
-
-        @Override
-        public int getGroupCount() {
-            return questions.length;
-        }
-
-        @Override
-        public int getChildrenCount(int questionPosition) {
-            return answers[questionPosition].length;
-        }
-
-        @Override
-        public Object getGroup(int questionPosition) {
-            return questions[questionPosition];
-        }
-
-        @Override
-        public Object getChild(int questionPosition, int answerPosition) {
-            return answers[questionPosition][answerPosition];
-        }
-
-        @Override
-        public long getGroupId(int questionPosition) {
-            return questionPosition;
-        }
-
-        @Override
-        public long getChildId(int questionPosition, int answerPosition) {
-            return answerPosition;
-        }
-
-        @Override
-        public boolean hasStableIds() {
-            return true;
-        }
-
-        @Override
-        public View getChildView(int questionPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-
-            ViewHolder holder;
-            View v = convertView;
-            if (v == null) {
-                v = inf.inflate(R.layout.list_item, parent, false);
-                holder = new ViewHolder();
-
-                holder.text = (TextView) v.findViewById(R.id.lblListItem);
-                v.setTag(holder);
-            } else {
-                holder = (ViewHolder) v.getTag();
-            }
-
-            holder.text.setClickable(true);
-            holder.text.setMovementMethod(LinkMovementMethod.getInstance());
-            holder.text.setText(Html.fromHtml(getChild(questionPosition, childPosition).toString(), Html.FROM_HTML_MODE_COMPACT));
-
-            return v;
-        }
-
-        @Override
-        public View getGroupView(int questionPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-            ViewHolder holder;
-            View v = convertView;
-            if (v == null) {
-                v = inf.inflate(R.layout.list_group, parent, false);
-
-                holder = new ViewHolder();
-                holder.text = (TextView) v.findViewById(R.id.lblListHeader);
-                v.setTag(holder);
-            } else {
-                holder = (ViewHolder) v.getTag();
-            }
-
-            holder.text.setText(getGroup(questionPosition).toString());
-
-            return v;
-        }
-
-        @Override
-        public boolean isChildSelectable(int questionPosition, int answerPosition) {
-            return true;
-        }
-
-        private class ViewHolder {
-            private TextView text;
-        }
-    }
 }
