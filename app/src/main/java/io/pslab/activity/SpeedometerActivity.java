df98944b341e239c50b46eacfaf12db796e5275d
Souma <souma.hitech@gmail.com>
2020-12-22 01:27:28 +0530

    Speedometer UI design complete

--- app/src/main/java/io/pslab/activity/SpeedometerActivity.java
+++ app/src/main/java/io/pslab/activity/SpeedometerActivity.java
@@ -1,15 +1,24 @@
 package io.pslab.activity;
 
-import android.content.Intent;
-import android.support.design.widget.BottomSheetBehavior;
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 import io.pslab.R;
 
-public class SpeedometerActivity extends AppCompatActivity {
+public class SpeedometerActivity extends AppCompatActivity implements LocationListener {
+    private static final int MY_PERMISSION_REQUEST_CODE=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +31,50 @@ public class SpeedometerActivity extends AppCompatActivity {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestRuntimeLocationPermission();
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
+        this.onLocationChanged(null);
+    }
+
+    private void requestRuntimeLocationPermission() {
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+        },MY_PERMISSION_REQUEST_CODE);
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        TextView textView = findViewById(R.id.speedometer_id);
+
+        if (location == null){
+            textView.setText("_,_ m/s");
+        }else {
+            float mCurrentSpeed = location.getSpeed();
+            textView.setText(mCurrentSpeed + "m/s");
+        }
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+        Toast.makeText(this, "GPS successfully turned on", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+        Toast.makeText(this, "Please make sure GPS is turn on", Toast.LENGTH_LONG).show();
     }
 
     @Override
