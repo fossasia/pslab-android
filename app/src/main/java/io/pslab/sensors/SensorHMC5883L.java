2fb906dafb40666ed529a0ed503a3d11e9759ba8
Souma <souma.hitech@gmail.com>
2020-12-27 01:29:29 +0530

    add app bar in hmc5883l activity

--- app/src/main/java/io/pslab/sensors/SensorHMC5883L.java
+++ app/src/main/java/io/pslab/sensors/SensorHMC5883L.java
@@ -4,6 +4,8 @@ import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
@@ -66,6 +68,14 @@ public class SensorHMC5883L extends AppCompatActivity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sensor_hmc5883l);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(R.string.hmc5883l);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
 
         sensorDock = (RelativeLayout) findViewById(R.id.sensor_control_dock_layout);
         indefiniteSamplesCheckBox = (CheckBox) findViewById(R.id.checkBox_samples_sensor);
@@ -298,5 +308,13 @@ public class SensorHMC5883L extends AppCompatActivity {
                 lock.notify();
             }
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return true;
     }
 }
