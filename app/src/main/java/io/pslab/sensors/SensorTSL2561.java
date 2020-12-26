db61757ed71acbddf215b653868f473ead8ae8a7
Souma <souma.hitech@gmail.com>
2020-12-27 02:55:12 +0530

    remove redundant property and modify dock UI in tsl2561 layout

--- app/src/main/java/io/pslab/sensors/SensorTSL2561.java
+++ app/src/main/java/io/pslab/sensors/SensorTSL2561.java
@@ -212,13 +212,13 @@ public class SensorTSL2561 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (play && scienceLab.isConnected()) {
-                    playPauseButton.setImageResource(R.drawable.play);
+                    playPauseButton.setImageResource(R.drawable.circle_play_button);
                     play = false;
                 } else if (!scienceLab.isConnected()) {
-                    playPauseButton.setImageResource(R.drawable.play);
+                    playPauseButton.setImageResource(R.drawable.circle_play_button);
                     play = false;
                 } else {
-                    playPauseButton.setImageResource(R.drawable.pause);
+                    playPauseButton.setImageResource(R.drawable.circle_pause_button);
                     play = true;
                     if (!indefiniteSamplesCheckBox.isChecked()) {
                         counter = Integer.parseInt(samplesEditBox.getText().toString());
@@ -319,7 +319,7 @@ public class SensorTSL2561 extends AppCompatActivity {
             samplesEditBox.setText(String.valueOf(counter));
             if (counter == 0 && !runIndefinitely) {
                 play = false;
-                playPauseButton.setImageResource(R.drawable.play);
+                playPauseButton.setImageResource(R.drawable.circle_play_button);
             }
             synchronized (lock) {
                 lock.notify();
