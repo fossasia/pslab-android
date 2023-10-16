f3982325b13c1a8c25e5b76be0c0b1ccee6902e2
Subhradeep <beradeep35@gmail.com>
2023-10-16 19:31:12 +0530

    Reformatted the code

--- app/src/main/java/io/pslab/activity/LuxMeterActivity.java
+++ app/src/main/java/io/pslab/activity/LuxMeterActivity.java
@@ -97,19 +97,17 @@ public class LuxMeterActivity extends PSLabSensor {
 
     @Override
     public void getDataFromDataLogger() {
-        if(SDK_INT >= 21) {
-            if (getIntent().getExtras() != null && getIntent().getExtras().getBoolean(KEY_LOG)) {
-                //playingData = true;
-                viewingData = true;
-                recordedLuxData = LocalDataLog.with()
-                        .getBlockOfLuxRecords(getIntent().getExtras().getLong(DATA_BLOCK));
-                final LuxData data = recordedLuxData.get(0);
-                if (data != null) {
-                    final String title = titleFormat.format(data.getTime());
-                    final ActionBar actionBar = getSupportActionBar();
-                    if (actionBar != null) {
-                        actionBar.setTitle(title);
-                    }
+        if (SDK_INT >= 21 && getIntent().getExtras() != null && getIntent().getExtras().getBoolean(KEY_LOG)) {
+            //playingData = true;
+            viewingData = true;
+            recordedLuxData = LocalDataLog.with()
+                    .getBlockOfLuxRecords(getIntent().getExtras().getLong(DATA_BLOCK));
+            final LuxData data = recordedLuxData.get(0);
+            if (data != null) {
+                final String title = titleFormat.format(data.getTime());
+                final ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) {
+                    actionBar.setTitle(title);
                 }
             }
         }
