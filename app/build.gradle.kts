2946be973ecba6b4af59ed913c8e1d41ab2800cf
Padmal <CloudyPadmal@users.noreply.github.com>
2024-02-26 15:48:57 +0100

    fix: updated app to run and compile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ val keystoreExists = System.getenv("KEYSTORE_FILE") != null
 
 android {
     namespace = "io.pslab"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "io.pslab"
-        minSdk = 21
-        targetSdk = 31
+        minSdk = 31
+        targetSdk = 34
         versionCode = 22
         versionName = "2.1.0"
     }
@@ -53,15 +53,16 @@ android {
     }
 }
 
+
 dependencies {
 
     // Android stock libraries
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.preference:preference:1.2.0")
-    implementation("androidx.browser:browser:1.5.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.preference:preference:1.2.1")
+    implementation("androidx.browser:browser:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     // Custom tools libraries
@@ -69,7 +70,7 @@ dependencies {
     implementation("com.github.bmelnychuk:atv:1.2.9")
     implementation("de.hdodenhof:circleimageview:3.1.0")
     implementation("com.github.devlight.navigationtabstrip:navigationtabstrip:1.0.4")
-    implementation("com.afollestad.material-dialogs:core:0.9.6.0")
+    implementation("com.afollestad.material-dialogs", "commons", "0.9.6.0")
     implementation("com.github.medyo:android-about-page:1.3.1")
     implementation("com.github.tiagohm.MarkdownView:library:0.19.0")
     implementation("com.github.mirrajabi:search-dialog:1.2.4")
@@ -77,13 +78,13 @@ dependencies {
     implementation("com.github.BeppiMenozzi:Knob:1.9.0")
     implementation("com.github.warkiz.widget:indicatorseekbar:2.1.2")
     implementation("com.github.Vatican-Cameos:CarouselPicker:1.2")
-    implementation("com.github.anastr:speedviewlib:1.6.0")
+    implementation("com.github.anastr:speedviewlib:1.6.1")
     implementation("com.github.GoodieBag:ProtractorView:v1.2")
     implementation("com.github.Triggertrap:SeekArc:v1.1")
 
     // Apache commons
     implementation("org.apache.commons:commons-math3:3.6.1")
-    implementation("org.apache.commons:commons-lang3:3.12.0")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
 
     // Picasso
     implementation("com.squareup.picasso:picasso:2.71828")
@@ -97,10 +98,13 @@ dependencies {
     implementation("com.jakewharton:butterknife:$butterKnifeVersion")
 
     // Map libraries
-    val osmDroidVersion = "6.1.11"
-    implementation("org.osmdroid:osmdroid-android:$osmDroidVersion")
-    implementation("org.osmdroid:osmdroid-mapsforge:$osmDroidVersion")
-    implementation("org.osmdroid:osmdroid-geopackage:$osmDroidVersion")
+    implementation("org.osmdroid:osmdroid-android:6.1.18")
+    implementation("org.osmdroid:osmdroid-mapsforge:6.1.18")
+    implementation("org.osmdroid:osmdroid-geopackage:6.1.18") {
+        exclude("org.osmdroid.gpkg");
+        exclude("ormlite-core");
+        exclude("com.j256.ormlite");
+    }
 
     // Realm
     implementation("io.realm:android-adapters:4.0.0")
